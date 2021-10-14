095917881806cd80f7f4755ffcdc53f357170a84
bitcamp123456 <tae7392@gmail.com>
2021-10-14 12:12:37 +0900

    exam

---
+++ java_project/FirstJava/src/test01/test01.java
@@ -0,0 +1,17 @@
+package test01;
+
+public class test01 {
+	public static void main(String[] args) {
+		String str1 = "My name ";
+		int str1length = str1.length();
+		System.out.println(str1.length());
+		
+		String str2 = "안녕 ";
+		int str2length = str2.length();
+		System.out.println(str2.length());
+		// 띄어쓰기도 문자열에 포함되는구나.
+		
+	
+	}
+
+}
