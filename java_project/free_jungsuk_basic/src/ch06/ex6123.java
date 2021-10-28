5b24fbc8dff627c239eab9d48df12e6ce7f13e47
bitcamp123456 <tae7392@gmail.com>
2021-10-28 19:59:54 +0900

    -

--- java_project/free_jungsuk_basic/src/ch06/ex6123.java
+++ java_project/free_jungsuk_basic/src/ch06/ex6123.java
@@ -1,21 +1,17 @@
 package ch06;
 
-public class ex63 {
+public class ex6123 {
+
 	public static void main(String[] args) {
-		Student s = new Student();
-		s.name = "홍길동";
-		s.ban = 1;
-		s.no = 1;
-		s.kor = 100;
-		s.eng = 60;
-		s.math = 76;
+		Student s = new Student("홍길동", 1, 1, 100, 76, 60);
+		
+		String str = s.info();
+		System.out.println(str);
 		
 		System.out.println(s.getTotal());
 		System.out.println(s.getAverage());
 		
-		
 	}
-	
 
 }
 
@@ -27,38 +23,32 @@ class Student {
 	int kor;
 	int eng;
 	int math;
-
-	public Student() {
-		super();
+	
+	public Student(String name, int ban, int no, int kor, int eng, int math) {
 		this.name = name;
 		this.ban = ban;
 		this.no = no;
 		this.kor = kor;
 		this.eng = eng;
 		this.math = math;
+		
 	}
-
+		
 	public String info() {
-		return name + "," + ban + "," + no + "," + kor + "," + eng + "," + math + "," + (kor + eng + math) + ","
-				+ ((int) ((kor + eng + math) / 3f * 10 + 0.5f) / 10f);
+		return name+","+ban+","+no+","+kor+","+eng+","+math+","+(kor+eng+math)+","+
+	(int)((kor+eng+math)/3f*10+0.5)/10f;
+		
 	}
 	
 	public int getTotal() {
-		return kor+eng+math;
+		return (kor+eng+math);
+		
 	}
 	
 	public float getAverage() {
-		return (int) ((kor+eng+math)/3f*10+0.5)/10f;
+		return (int) ((kor+eng+math)/3f*100+0.5)/100f;
 		
 	}
 	
 	
-
 }
-
-
-
-
-
-
-
--- java_project/free_jungsuk_basic/src/ch06/ex64Q.java
+++ java_project/free_jungsuk_basic/src/ch06/ex64Q.java
@@ -1,9 +1,10 @@
 package ch06;
 
 public class ex64Q {
-	// (x,y) (x1,y1) . 두 점 와 간의 거리를 구한다
-	static double getDistance(int x, int y, int x1, int y1) {
+	static // (x,y) (x1,y1) . 두 점 와 간의 거리를 구한다
+	 double getDistance(int x, int y, int x1, int y1) {
 		return Math.sqrt((x-x1)*(x-x1)+(y-y1)*(y-y1));
+		
 		
 
 	}
@@ -11,4 +12,9 @@ public class ex64Q {
 	public static void main(String args[]) {
 		System.out.println(getDistance(1, 1, 2, 2));
 	}
+	
 }
+
+
+
+// ABC123
