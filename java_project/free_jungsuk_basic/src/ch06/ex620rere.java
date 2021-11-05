ec9977d630db1c1d0c5881ac464709f2d99c63ad
bitcamp123456 <tae7392@gmail.com>
2021-11-05 18:20:18 +0900

    -

--- java_project/free_jungsuk_basic/src/ch06/ex620rere.java
+++ java_project/free_jungsuk_basic/src/ch06/ex620rere.java
@@ -1,5 +1,41 @@
 package ch06;
 
 public class ex620rere {
+	
+	static int max(int[] arr) {
+		if(arr==null||arr.length==0) {
+			return -999999;
+		}
+		
+		int max = arr[0];
+		
+		for(int i=1;i<arr.length;i++) {
+			if(arr[i]>max) {
+				max = arr[i];
+				
+			}
+			
 
+		}
+		return max;
+
+
+
+
+		
+
+	}
+	
+	public static void main(String[] args) {
+		int[] data = {1,2,100,50,4,-2,5000};
+		
+		System.out.println(max(data));
+	}
+
+	
+	
+	
+	
 }
+
+
---
+++ java_project/free_jungsuk_basic/src/ch06/ex621rere.java
@@ -0,0 +1,18 @@
+package ch06;
+
+public class ex621rere {
+	
+	static int abs(int value) {
+		
+		return value>=0 ? value : -value;
+
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		int value = 5;
+		System.out.println(abs(value));
+	}
+
+}
