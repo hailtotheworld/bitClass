3eed5708ad91cf14c8ea3577d7787fc7377a21c8
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2021-11-16 08:10:02 +0900

    -

---
+++ java_jungsuk_basic/ch03/src/Ex3_11.java
@@ -0,0 +1,19 @@
+class Ex3_11 {
+	public static void main(String args[]) { 
+		double pi = 3.141592; 
+		
+		System.out.println(pi);
+		System.out.println(pi*1000);
+		System.out.println(Math.round(pi*1000));
+		System.out.println(Math.round(pi*1000)/1000); // 3
+		System.out.println((double) Math.round(pi*1000)/1000); // 3.142
+		System.out.println(Math.round(pi*1000)/1000.0); // 3.142
+		System.out.println();
+//		double shortPi = Math.round(pi * 1000) / 1000.0;
+//		System.out.println(shortPi);
+		
+		System.out.println(pi*1000);
+		System.out.println((int)(pi*1000));
+		System.out.println((int)(pi*1000)/1000.0);
+	}
+}
