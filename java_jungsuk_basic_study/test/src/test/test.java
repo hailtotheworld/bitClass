5d6140e187cac76ecc585745deab339c8e98efa2
bitcamp123456 <tae7392@gmail.com>
2021-11-17 09:35:35 +0900

    -

---
+++ java_jungsuk_basic_study/test/src/test/test.java
@@ -0,0 +1,16 @@
+package test;
+
+public class test {
+	public static void main(String[] args) {
+		double a = 3.889;
+
+		
+		System.out.println(Math.round(a)); // 반올림
+		System.out.println(Math.round(a*100)/100.0); // 특정자리 반올림
+		System.out.println();
+
+	}
+	
+	
+
+}
