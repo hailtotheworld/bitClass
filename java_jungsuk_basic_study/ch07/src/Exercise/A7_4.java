4c9eaa6408a271ff5f5016d36f80444f017d2ec6
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2021-12-13 07:57:32 +0900

    -

---
+++ java_jungsuk_basic_study/ch07/src/Exercise/A7_4.java
@@ -0,0 +1,60 @@
+package Exercise;
+
+class MyTv {
+	private boolean isPowerOn;
+	private int channel;
+	private int volume;
+	final int MAX_VOLUME = 100;
+	final int MIN_VOLUME = 0;
+	final int MAX_CHANNEL = 100;
+	final int MIN_CHANNEL = 1;
+
+	public boolean isPowerOn() {
+		return isPowerOn;
+	}
+
+	public void setPowerOn(boolean isPowerOn) {
+		this.isPowerOn = isPowerOn;
+	}
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public void setChannel(int channel) {
+		if (channel > MAX_CHANNEL || channel < MIN_CHANNEL)
+			return;
+		this.channel = channel;
+
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		if (volume > MAX_VOLUME || volume < MIN_VOLUME)
+			return;
+		this.volume = volume;
+	}
+
+//	int gotoPrevChannel() {
+//		int preChannel;
+//		return channel = preChannel;
+//	}
+
+}
+
+class A7_4 {
+	public static void main(String args[]) {
+		MyTv t = new MyTv();
+		t.setChannel(50);
+		System.out.println("CH:" + t.getChannel());
+		t.setChannel(300);
+		System.out.println("CH:" + t.getChannel());
+//		t.gotoPrevChannel();
+//		System.out.println("CH:" + t.getChannel());
+//		t.gotoPrevChannel();
+//		System.out.println("CH:" + t.getChannel());
+	}
+}
---
+++ java_jungsuk_basic_study/ch07/src/Exercise/Exercise7_4.java
@@ -0,0 +1,22 @@
+package Exercise;
+
+//class MyTv {
+//	boolean isPowerOn;
+//	int channel;
+//	int volume;
+//	final int MAX_VOLUME = 100;
+//	final int MIN_VOLUME = 0;
+//	final int MAX_CHANNEL = 100;
+//	final int MIN_CHANNEL = 1;
+//
+//	
+//
+//class Exercise7_4 {
+//	public static void main(String args[]) {
+//		MyTv t = new MyTv();
+//		t.setChannel(10);
+//		System.out.println("CH:" + t.getChannel());
+//		t.setVolume(20);
+//		System.out.println("VOL:" + t.getVolume());
+//	}
+//}
