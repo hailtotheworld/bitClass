35799e5245580251c58d992b7846abee7f2b15d7
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-03-24 14:46:56 +0900

    java

---
+++ [Java]/JavaPrj/src/Program.java
@@ -0,0 +1,23 @@
+public class Program {
+	public static void main(String[] args) {
+
+		int kor1, kor2, kor3;
+		int total;
+		float avg;
+
+		kor1 = 50;
+		kor2 = 60;
+		kor3 = 80;
+
+		total = kor1 + kor2 + kor3;
+
+		avg = (float) total / 3;
+
+		System.out.printf("total: %d%n", total);
+		System.out.printf("avg: %.2f", avg);
+
+
+
+	}
+
+}
