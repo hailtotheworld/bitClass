c4e6839fde0ac2642259d3f18a746dfe8c69d9ef
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-03-25 20:11:56 +0900

    Update Program.java

--- [Java]/JavaPrj/src/ex4/io/printf/Program.java
+++ [Java]/JavaPrj/src/ex4/io/printf/Program.java
@@ -12,23 +12,28 @@ public class Program {
 
 		total = kor1 + kor2 + kor3;
 
-		avg = (float) total / 3;
+//		avg = total / 3.0f;
+//		avg = (float) total / 3;
+		avg = (float) (total / 3.0);
 
 		// ------ 성적 출력 부분 ---------------------------------------
 		System.out.println("┌──────────────────────┓");
 	    System.out.println("│        성적 출력                │");
 		System.out.println("└──────────────────────┘");
 				
-		System.out.println("\t국어1 : 0");
-		System.out.println("\t국어2 : 0");
-		System.out.println("\t국어3 : 0");
-		System.out.println("\t총점 : 0");
-		System.out.println("\t평균 : 0.00");
+		System.out.printf("\t국어1 : %3d\n ",kor1);
+		System.out.printf("\t국어2 : %3d\n", kor2);
+		System.out.printf("\t국어3 : %3d\n", kor3);
+		System.out.printf("\t총점 : %3d\n", total);
+		System.out.printf("\t평균 : %6.2f\n", avg);
 		System.out.println("────────────────────────");
 		
+		System.out.printf("%2$d %3$d %1$d\n",1,2,3);
+		System.out.printf("%1$d %1$d %1$d\n",1);
+				
 //		System.out.print(80);
-		System.out.write(80);
-		System.out.flush(); 
+//		System.out.write(80);
+//		System.out.flush(); 
 
 
 
