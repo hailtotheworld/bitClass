b2fcba1b742cb0f25eee53118dc3f6c4b587aa2a
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-03-29 23:34:27 +0900

    java

--- [Java]/StructuredJavaPrj/src/part2/ex2/탑다운예제/Program.java
+++ [Java]/StructuredJavaPrj/src/part2/ex2/탑다운예제/Program.java
@@ -1,11 +1,53 @@
 package part2.ex2.탑다운예제;
 
-
 public class Program {
 
 	public static void main(String[] args) {
 
+		int[][] lottos = null;
+		int menu; // 메뉴를 입력받기위한 변수
+		boolean running = true;
 
+		while (running) {
+			menu = inputMenu(); // 메뉴를 입력받기위한 함수
+
+			switch (menu) {
+			case 1:
+				lottos = createLottosAuto(); // create: 공간까지 만든다 / generate: 번호만 만든다
+				break;
+			case 2:
+				lottos = createLottosManual();
+				break;
+			case 3:
+				printLottos(lottos);
+				break;
+			case 4:
+				running = false;
+				break;
+			default:
+			}
+		}
+
+	}
+
+	private static void printLottos(int[][] lottos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static int[][] createLottosManual() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static int[][] createLottosAuto() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static int inputMenu() {
+
+		return 0;
 	}
 
 }
---
+++ [Java]/StructuredJavaPrj/src/part2/ex3/데이터구조화/Exam.java
@@ -0,0 +1,8 @@
+package part2.ex3.데이터구조화;
+
+public class Exam { // 여기서는 그저 설명서를 만드는 거구나
+	int kor;
+	int eng;
+	int math;
+
+}
