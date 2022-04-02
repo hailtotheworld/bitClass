1338e51f8595fdb9936c382a4885cebc5dcb87c9
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-04-02 18:48:30 +0900

    Update Program.java

--- [Java]/OOPJavaPrj/src/Program.java
+++ [Java]/OOPJavaPrj/src/Program.java
@@ -1,8 +1,11 @@
+import part3.ex4.UI코드분리하기.Exam;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Exam exam = new Exam();
+		Exam exam = new Exam(10,10,11);
+		
+		System.out.println(exam.total());
 		
 	}
 
