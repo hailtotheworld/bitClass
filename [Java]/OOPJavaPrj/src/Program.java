6fc9e59a3715caaa9861ce5e0af280ae71e7ad96
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-04-03 09:20:21 +0900

    java

--- [Java]/OOPJavaPrj/src/Program.java
+++ [Java]/OOPJavaPrj/src/Program.java
@@ -3,7 +3,12 @@ import part3.ex4.UI코드분리하기.Exam;
 public class Program {
 
 	public static void main(String[] args) {
-		Exam exam = new Exam(10,10,11);
+		
+		NewlecExam exam = new NewlecExam();
+		exam.setKor(10);
+		exam.setEng(10);
+		exam.setMath(10);
+		exam.setCom(10);
 		
 		System.out.println(exam.total());
 		
