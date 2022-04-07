3ca967f3cb743ee2f64e5aa5e28e52ef79d1fe4c
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-04-07 12:23:47 +0900

    java

---
+++ [Java]/ExceptionPrj/src/Calculator.java
@@ -0,0 +1,34 @@
+
+public class Calculator {
+//	private int x;
+//	private int y;
+
+	public Calculator() {
+	}
+
+//	public Calculator(int x, int y) {
+//		this.x = x;
+//		this.y = y;
+//	}
+
+//	public int add() {
+//		return x + y;
+//	}
+
+	public static int add(int x, int y) {
+		return x + y;
+	}
+
+	public static int sub(int x, int y) {
+		return x - y;
+	}
+
+	public static int multi(int x, int y) {
+		return x*y;
+	}
+
+	public static int div(int x, int y) {
+		return x/y;
+	}
+
+}
---
+++ [Java]/ExceptionPrj/src/Program.java
@@ -0,0 +1,15 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Calculator calc = new Calculator();
+		
+		int result = 0;
+		result = Calculator.add(3,4);
+		result = Calculator.sub(3,4);
+		result = Calculator.multi(3,4);
+		result = Calculator.div(3,4);
+	}
+
+}
