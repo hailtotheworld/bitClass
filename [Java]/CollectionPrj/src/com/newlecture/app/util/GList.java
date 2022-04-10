610bd251a7fac7e88f4ac9424f39cad5a0af5f8d
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-04-10 11:30:08 +0900

    java

--- [Java]/CollectionPrj/src/com/newlecture/app/util/GList.java
+++ [Java]/CollectionPrj/src/com/newlecture/app/util/GList.java
@@ -3,14 +3,32 @@ package com.newlecture.app.util;
 public class GList<T> {
 
 	private Object[] nums;
-	private int current;
+	private int current; // = 0
+	private int capacity; // 용량
+	private int amount; // 추가증가량
 
 	public GList() {
 		nums = new Object[3];
 		current = 0;
+		capacity = 3;
+		amount = 5;
 	}
 
 	public void add(T num) {
+
+//		if(용량이 부족하다면)
+//			용량을 늘려
+
+		if (capacity <= current) {
+			Object[] temp = new Object[capacity + amount];
+			
+			for (int i = 0; i < current; i++) {
+				temp[i] = nums[i];
+				nums = temp;
+				capacity += amount;
+			}
+		}
+
 		nums[current] = num;
 		current++;
 	}
--- [Java]/CollectionPrj/src/com/newlecture/app/util/Program.java
+++ [Java]/CollectionPrj/src/com/newlecture/app/util/Program.java
@@ -9,18 +9,26 @@ public class Program {
 		list.add(5);
 		int size = list.size();
 		System.out.printf("size : %d\n", size);
-		
+
 		list.clear();
 		size = list.size();
 		System.out.printf("size : %d\n", size);
-		
+
 		list.add(7);
-		int num = list.get(0);
+		list.add(8);
+		list.add(9);
+		list.add(10);
+		list.add(11);
+		list.add(12);
+
+		int num = list.get(3);
 		// Object형을 반환되고 있으니까 형변환해줘야지
 		// Integer로 형변환하면 AutoUnboxing되서 Int로 바뀐다.
 		System.out.printf("num : %d\n", num);
 //		num = (Integer) list.get(1);
-
+		
+		System.out.println(list.size());
+		
 
 	}
 
