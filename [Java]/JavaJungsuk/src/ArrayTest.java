ce9a482571cace3f48c5c8c6995d095c6d6b010a
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-04-22 22:26:21 +0900

    Update ArrayTest.java

--- [Java]/JavaJungsuk/src/ArrayTest.java
+++ [Java]/JavaJungsuk/src/ArrayTest.java
@@ -3,14 +3,13 @@ import java.util.Arrays;
 public class ArrayTest {
 	public static void main(String[] args) {
 
-		int[][] arr = new int[][]{{1,2,3}, {4,5},{6,7,8,9}};
+		int[][] arr = new int[][] { { 1, 2, 3 }, { 4, 5 }, { 6, 7, 8, 9 } };
 		System.out.println(arr[0].toString()); // 주소가 나온다.
 		System.out.println(Arrays.toString(arr[0])); // [1, 2, 3]
-		
-		int[] arr2 = new int[] {1,2,3,7};
+
+		int[] arr2 = new int[] { 1, 2, 3, 7 };
 		System.out.println(arr2.toString());
 		System.out.println(Arrays.toString(arr2));
-
 
 	}
 }
