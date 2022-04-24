9fe5df8377a2065897445d602becea10514fee0f
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-04-24 22:34:33 +0900

    Create superTest.java

---
+++ [Java]/JavaJungsuk/src/superTest.java
@@ -0,0 +1,34 @@
+abstract class Vehicle {
+	String name;
+
+	abstract public String getName(String val);
+
+	public String getName() {
+		return "Vehicle name:" + name;
+	}
+}
+
+class Car1 extends Vehicle {
+	String name;
+
+	public Car1(String val) {
+		name = super.name = val;
+	}
+
+	public String getName(String val) {
+		return "Car name:" + val;
+	}
+
+	public String getName(byte val[]) {
+		return "Car name:" + val;
+	}
+
+}
+
+public class superTest {
+	public static void main(String args[]) {
+		Vehicle obj = new Car1("Spark");
+		System.out.print(obj.getName());
+		
+	}
+}
