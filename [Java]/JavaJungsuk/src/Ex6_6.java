04e689568bdfc228b842b438ec3a6c83bf82f1c1
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-05-10 20:38:35 +0900

    java

--- [Java]/JavaJungsuk/src/Ex6_6.java
+++ [Java]/JavaJungsuk/src/Ex6_6.java
@@ -6,16 +6,16 @@ class Ex6_6 {
 		d.x = 10;
 		System.out.println("main() : x = " + d.x);
 
-		change(d.x);
+//		change(d.x);
 		change(d);
 		System.out.println("After change(d.x)");
 		System.out.println("main() : x = " + d.x);
 	}
 
-	static void change(int k) {  // 기본형 매개변수
-		k = 1000;
-		System.out.println("change() : k = " + k);
-	}
+//	static void change(int k) {  // 기본형 매개변수
+//		k = 1000;
+//		System.out.println("change() : k = " + k);
+//	}
 	static void change(Data d) {  // 참조형 매개변수
 		d.x = 1000;
 		System.out.println("change() : x = " + d.x);
