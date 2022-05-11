4abc81d731452805e886c066be7cd2b8c7bc28fc
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-05-11 12:29:24 +0900

    Update OverrideTest.java

--- [Java]/JavaJungsuk/src/OverrideTest.java
+++ [Java]/JavaJungsuk/src/OverrideTest.java
@@ -2,36 +2,27 @@ class Point {
 	int x;
 	int y;
 
-//	String getLocation() {
-//		return "x="+x+",y="+y;
-//	}
-
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return "x=" + x + ",y=" + y;
 	}
 
-//	void getLocation1() {
-//		System.out.printf("x=%d,y=%d",x,y);
-//	}
 }
 
 class Point3D extends Point {
 	int z;
 
-//	@Override
-//	String getLocation() {
-//		return super.getLocation()+",z="+z;
-//	}
+	public Point3D() {
+	}
 
-//	String getLocation() {
-//		return "x="+x+",y="+y+",z="+z;
-//	}
+	public Point3D(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return super.toString() + ",z=" + z;
 	}
 
@@ -41,14 +32,9 @@ public class OverrideTest {
 
 	public static void main(String[] args) {
 
-		Point3D p = new Point3D();
-		p.x = 7;
-		p.y = 9;
-		p.z = 11;
+		Point3D p = new Point3D(7, 9, 11);
 
-//		System.out.println(p.getLocation());
 		System.out.println(p);
-		System.out.println(p.toString());
 
 	}
 
