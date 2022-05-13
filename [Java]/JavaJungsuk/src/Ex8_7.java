1956d8c5589a54cf675ffdb04ca3cf30a136def4
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-05-14 01:13:01 +0900

    Update Ex8_7.java

--- [Java]/JavaJungsuk/src/Ex8_7.java
+++ [Java]/JavaJungsuk/src/Ex8_7.java
@@ -1,14 +1,23 @@
 class Ex8_7 {
 	public static void main(String[] args) {
 
+		// checked에러라서 컴파일러가 예외처리여부를 체크한다.
+		// 예외처리 안하면 빨간줄뜨면서 실행조차 안됨 <- 컴파일에러 발생했다는 의미이다.
+//		throw new Exception();
+		
 		try {
-			throw new Exception("예외처리필수"); // 컴파일에러가 발생함. 예외처리 필수야. 예외처리 안하면 실행조차 안돼.
+			throw new Exception();
 		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println(e.getMessage());
+			System.out.println("예외처리1");
 		}
 
-		throw new RuntimeException(); // 컴파일에러가 없음. 예외처리 선택적이라는거지.
+		// unchecked에러라서 컴파일러가 예외처리여부를 체크안한다.
+		// 실행은 되는데 실행하면 런타임에러발생함.
 
+		try {
+			throw new RuntimeException();
+		} catch (Exception e) {
+			System.out.println("예외처리2");
+		}
 	}
 }
