7b4f161aeff5e49373a16081d5312e9204b5b161
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-05-19 00:38:01 +0900

    Update typeTest.java

--- [Java]/JavaJungsuk/src/ch02/typeTest.java
+++ [Java]/JavaJungsuk/src/ch02/typeTest.java
@@ -2,14 +2,22 @@ package ch02;
 
 public class typeTest {
 	public static void main(String[] args) {
-		// 숫자 → 문자
-		int x=6_5;
 		
-		char c = Character.valueOf((char) x);
+		long l = 10_000_000_000L;
 		
-		System.out.println(c);
 		
-		long l = 10000000000L;
+		double d = 2e3;
+		System.out.println(d);
+		
+		
+		int b = 0b10;
+		int oct = 010;
+		int hex = 0x10;
+		System.out.println(b+","+oct+","+hex);
+		
+		System.out.printf("%x%n",hex);
+		System.out.printf("%x%n",16);
+		System.out.printf("%o%n",50);
 		
 	}
 
