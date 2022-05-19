88b5cb06de27e4e57917b83847e40fb31e27946b
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-05-20 00:25:53 +0900

    Update calendarTest.java

--- [Java]/JavaJungsuk/src/ch10/calendarTest.java
+++ [Java]/JavaJungsuk/src/ch10/calendarTest.java
@@ -7,8 +7,21 @@ public class calendarTest {
 		
 		Calendar today = Calendar.getInstance();
 		
+		
 		System.out.println(today.get(Calendar.YEAR));
+		System.out.println(today.get(today.YEAR));
+		System.out.println(today.YEAR);
 		System.out.println(Calendar.YEAR);
+		
+		ss s1 = new ss();
+		ss s2 = new ss();
+		
+		ss.X=15;
 	}
 
+}
+
+class ss {
+	static int X = 10;
+	int v;
 }
