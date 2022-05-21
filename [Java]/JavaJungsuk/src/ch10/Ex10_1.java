89c9946124d64c85720d397357115efd6dd02b13
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-05-21 22:16:45 +0900

    Update Ex10_1.java

--- [Java]/JavaJungsuk/src/ch10/Ex10_1.java
+++ [Java]/JavaJungsuk/src/ch10/Ex10_1.java
@@ -10,13 +10,17 @@ class Ex10_1 {
 		System.out.println(1 + today.get(Calendar.MONTH)+ " <- 월(0~11, 0:1월)");
 		System.out.println("이 해의 몇 째 주: " + today.get(Calendar.WEEK_OF_YEAR));
 		System.out.println("이 달의 몇 째 주: " + today.get(Calendar.WEEK_OF_MONTH));
+		
 		// DATE와 DAY_OF_MONTH는 같다.
 		System.out.println("이 달의 몇 일: " + today.get(Calendar.DATE));
 		System.out.println("이 달의 몇 일: " + today.get(Calendar.DAY_OF_MONTH));
+		
 		System.out.println("이 해의 몇 일: " + today.get(Calendar.DAY_OF_YEAR));
 		System.out.println("요일(1~7, 1:일요일): " + today.get(Calendar.DAY_OF_WEEK)); // 1:일요일, 2:월요일, ... 7:토요일
 		System.out.println("이 달의 몇 째 요일: " + today.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+		
 		System.out.println("오전_오후(0:오전, 1:오후): " + today.get(Calendar.AM_PM));
+		
 		System.out.println("시간(0~11): " + today.get(Calendar.HOUR));
 		System.out.println("시간(0~23): " + today.get(Calendar.HOUR_OF_DAY));
 		System.out.println("분(0~59): " + today.get(Calendar.MINUTE));
