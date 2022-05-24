bb26e0006b63baebfdaf6759d9af556c6a97be91
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-05-25 02:26:56 +0900

    java

--- [Java]/JavaJungsuk/src/ch11/Ex11_10.java
+++ [Java]/JavaJungsuk/src/ch11/Ex11_10.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class Ex11_10 {
 	public static void main(String[] args) {
-		Set set = new HashSet();
+		Set set = new LinkedHashSet();
 		
 		for (int i = 0; set.size() < 6 ; i++) {
 			int num = (int)(Math.random()*45) + 1;
@@ -12,8 +12,9 @@ class Ex11_10 {
 			set.add(num);
 		}
 
+		
 		List list = new LinkedList(set); // LinkedList(Collection c)
-		Collections.sort(list);          // Collections.sort(List list)
+		Collections.sort(list, Collections.reverseOrder());          // Collections.sort(List list)
 		System.out.println(list);
 		
 		
--- [Java]/JavaJungsuk/src/ch11/sortTest.java
+++ [Java]/JavaJungsuk/src/ch11/sortTest.java
@@ -56,6 +56,7 @@ public class sortTest {
 
 		System.out.println(list);
 
+		
 
 		
 		
