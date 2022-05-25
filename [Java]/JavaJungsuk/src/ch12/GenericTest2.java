3ffbf8d1d3f242f555aea45232f2f61232da3ddc
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-05-26 00:45:24 +0900

    Update GenericTest2.java

--- [Java]/JavaJungsuk/src/ch12/GenericTest2.java
+++ [Java]/JavaJungsuk/src/ch12/GenericTest2.java
@@ -6,12 +6,12 @@ import java.util.ArrayList;
 class Tv {}
 class Audio {}
 
-
 public class GenericTest2 {
 	public static void main(String[] args) {
 		ArrayList<Tv> list = new ArrayList<Tv>();
+		
 		list.add(new Tv());
-		list.add(new Audio());
+//		list.add(new Audio());
 	}
 
 }
