9c3efadcfe34cde7d6a34674615793b4c43a7ca7
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-05-26 15:32:41 +0900

    java

--- [Java]/JavaJungsuk/src/ch12/GenericTest2.java
+++ [Java]/JavaJungsuk/src/ch12/GenericTest2.java
@@ -3,8 +3,8 @@ package ch12;
 import java.util.ArrayList;
 
 
-class Tv {}
-class Audio {}
+class Tv1 {}
+class Audio1 {}
 
 public class GenericTest2 {
 	public static void main(String[] args) {
@@ -12,11 +12,11 @@ public class GenericTest2 {
 		ArrayList list = new ArrayList(); // Tv타입의 객체만 저장가능
 //		ArrayList<Tv> list = new ArrayList<Tv>(); // Tv타입의 객체만 저장가능
 		
-		list.add(new Tv());
+		list.add(new Tv1());
 //		list.add(new Audio());
 		
 		
-		Tv t = (Tv) list.get(0);
+		Tv1 t = (Tv1) list.get(0);
 	}
 
 }
