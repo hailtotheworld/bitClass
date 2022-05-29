1114e177d2d3f3c94ae24b63f7f84f0d2517f6f8
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-05-29 18:44:36 +0900

    Update Ex12_8.java

--- [Java]/JavaJungsuk/src/ch12/Ex12_8.java
+++ [Java]/JavaJungsuk/src/ch12/Ex12_8.java
@@ -9,9 +9,11 @@ import static java.lang.annotation.ElementType.TYPE;
 
 import java.lang.annotation.*;
 
-@Deprecated
-@SuppressWarnings("1111") // 유효하지 않은 애너테이션은 무시된다.
-@TestInfo(testedBy="aaa", testDate=@DateTime(yymmdd="160101",hhmmss="235959"))
+import org.junit.jupiter.api.Test;
+
+//@Deprecated
+//@SuppressWarnings("1111") // 유효하지 않은 애너테이션은 무시된다.
+@TestInfo(testedBy="aaa", testTools = {"JUnit","AutoTester"}, testDate=@DateTime(yymmdd="160101",hhmmss="235959"))
 class Ex12_8 {
 	public static void main(String args[]) {
 		// Ex12_8의 Class객체를 얻는다.
@@ -36,13 +38,12 @@ class Ex12_8 {
 }
 
 @Retention(RetentionPolicy.RUNTIME)  // 실행 시에 사용가능하도록 지정 
-//@Retention(RetentionPolicy.SOURCE)  // 실행 시에 사용가능하도록 지정 
 @interface TestInfo {
 	int       count()	  	default 1; // default값이 있는것들은 사용할 때 적지않아도 된다. defalut말고 다른 값주고 싶으면 직접 적어주면 된다.
 	String    testedBy();
 	String[]  testTools() 	default {"JUnit","JUnit5"};
-	TestType  testType()    default TestType.FIRST;
-	DateTime  testDate();
+	TestType  testType()    default TestType.FIRST; // enum타입도 가능하다.
+	DateTime  testDate(); // 자신이 아닌 다른 애너테이션을 포함할 수 있다.
 }
 
 @Retention(RetentionPolicy.RUNTIME)  // 실행 시에 사용가능하도록 지정
@@ -52,6 +53,10 @@ class Ex12_8 {
 }
 
 enum TestType { FIRST, FINAL }
+
+
+
+
 
 
 
