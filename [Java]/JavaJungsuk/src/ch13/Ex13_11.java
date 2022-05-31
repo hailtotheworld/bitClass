78202e1708bb2ef65f3913033a0c6c4fcc0745c5
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-05-31 17:29:25 +0900

    java

--- [Java]/JavaJungsuk/src/ch13/Ex13_11.java
+++ [Java]/JavaJungsuk/src/ch13/Ex13_11.java
@@ -8,7 +8,7 @@ class Ex13_11 {
 		ThreadEx11_2 th2 = new ThreadEx11_2();
 		th1.start();
 		th2.start();
-		startTime = System.currentTimeMillis();
+		startTime = System.currentTimeMillis(); // 시작시간
 
 		try {
 			th1.join();	// main쓰레드가 th1의 작업이 끝날 때까지 기다린다.
@@ -16,6 +16,7 @@ class Ex13_11 {
 		} catch(InterruptedException e) {}
 
 		System.out.print("소요시간:" + (System.currentTimeMillis() - Ex13_11.startTime));
+		
 	} // main
 }
 
