fc92271de6929f327eee3d208b22165c7e248a61
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-06-10 20:04:09 +0900

    Create Ex14_2.java

---
+++ [Java]/JavaJungsuk/src/ch14/Ex14_2.java
@@ -0,0 +1,45 @@
+package ch14;
+
+import java.util.function.*;
+import java.util.*;
+
+class Ex14_2 {
+	public static void main(String[] args) {
+		Supplier<Integer>  s = ()-> (int)(Math.random()*100)+1; // 1~100난수를 반환
+		Consumer<Integer>  c = i -> System.out.print(i+", ");  // console에 출력
+		Predicate<Integer> p = i -> i%2==0; // 짝수인지 검사
+		Function<Integer, Integer> f = i -> i/10*10; // i의 일의 자리를 없앤다.
+		
+		List<Integer> list = new ArrayList<>();	
+		makeRandomList(s, list); // list를 랜덤값으로 채운다
+		System.out.println(list);
+		printEvenNum(p, c, list);
+		List<Integer> newList = doSomething(f, list);
+		System.out.println(newList);
+	}
+
+	static <T> List<T> doSomething(Function<T, T> f, List<T> list) {
+		List<T> newList = new ArrayList<T>(list.size());
+
+		for(T i : list) {
+			newList.add(f.apply(i));
+		}	
+
+		return newList;
+	}
+
+	static <T> void printEvenNum(Predicate<T> p, Consumer<T> c, List<T> list) {
+		System.out.print("[");
+		for(T i : list) {
+			if(p.test(i)) // 짝수인지 검사
+				c.accept(i);
+		}	
+		System.out.println("]");
+	}
+
+	static <T> void makeRandomList(Supplier<T> s, List<T> list) {
+		for(int i=0;i<10;i++) {
+			list.add(s.get()); // Supplier로 부터 1~100의 난수를 받아서 list에 추가
+		}
+	}
+}
