4b5f2c33c1b29d37bc27d3acd3eb9ad1dbeadf45
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-06-14 20:05:42 +0900

    Create Ex14_9.java

---
+++ [Java]/JavaJungsuk/src/ch14_2_Stream/Ex14_9.java
@@ -0,0 +1,64 @@
+package ch14_2_Stream;
+
+import java.util.*;
+import java.util.stream.*;
+
+class Ex14_9 {
+	public static void main(String[] args) {
+		String[] strArr = {
+			"Inheritance", "Java", "Lambda", "stream",
+			"OptionalDouble", "IntStream", "count", "sum"
+		};
+
+		Stream.of(strArr)
+		.parallel() // 병렬로 처리-여러스레드가 나눠서 처리해서 순서가 바뀐다
+		.forEachOrdered(System.out::println); // .forEachOrdered()를 쓰면 병렬처리해도 순서유지된다.
+		
+		System.out.println();
+		//////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		boolean noEmptyStr = Stream.of(strArr).noneMatch(s->s.length()==0);
+//		Optional<String> sWord = Stream.of(strArr)
+//					               .filter(s->s.charAt(0)=='s').findFirst(); // stream
+		
+		Optional<String> sWord = Stream.of(strArr).parallel()
+	               .filter(s->s.charAt(0)=='s').findAny(); // stream 또는 sum
+
+		System.out.println("noEmptyStr="+noEmptyStr);
+		System.out.println("sWord="+ sWord.get());
+		
+		
+		System.out.println();
+		//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+		// Stream<String>을 IntStream으로 변환
+		IntStream intStream1 = Stream.of(strArr).mapToInt(String::length);
+		IntStream intStream2 = Stream.of(strArr).mapToInt(String::length);
+		IntStream intStream3 = Stream.of(strArr).mapToInt(String::length);
+		IntStream intStream4 = Stream.of(strArr).mapToInt(String::length);
+
+		int count = intStream1.reduce(0, (a,b) -> a + 1);
+		int sum   = intStream2.reduce(0, (a,b) -> a + b);
+
+		OptionalInt max = intStream3.reduce(Integer::max);
+		OptionalInt min = intStream4.reduce(Integer::min);
+		System.out.println("count="+count);
+		System.out.println("sum="+sum);
+		System.out.println("max="+ max.getAsInt());
+		System.out.println("min="+ min.getAsInt());
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
