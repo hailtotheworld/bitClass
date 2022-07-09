7a5434bc5be8a90076660d90ffca689b1e1cfa2f
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-07-09 21:30:08 +0900

    js

---
+++ (HTML,CSS,JS)/[HTML,CSS,JS] 유노코딩/3 JS/script.js
@@ -0,0 +1,22 @@
+// window.alert('경고발생')
+// window.alert('경고발생22')
+// window.alert('경고발생333')
+
+
+// 한줄짜리 주석
+/* 여러줄 주석 */
+
+
+// window.console.log('기록남는다') // 콘솔화면에 기록을 남기겠다는 의미이다.
+console.log('console접근할때 window.은 생략이가능하다.'); console.log('세미콜론이 있으면 구문이 하나 끝난거라서 한줄에 코드를 이어서쓸수있다')
+console.log('세미콜론;은 한줄의 코드에 두 줄 이상의 구문이 있을때는 반드시 필요하다.')
+console.log('한줄에 한구문만 있을때는 세미콜론을 굳이 써주지 않아도 된다. 선택이지.')
+
+console.clear() // console을 깨끗하게 지워준다. log로 기록해둔게 다 지워졌네.
+
+
+
+
+
+
+
