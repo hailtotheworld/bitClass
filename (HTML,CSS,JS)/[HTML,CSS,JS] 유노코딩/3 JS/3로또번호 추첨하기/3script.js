0524e12d4ee4d3327fdd99c51cb5129570fe11fa
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-07-13 00:06:51 +0900

    Update 3script.js

--- (HTML,CSS,JS)/[HTML,CSS,JS] 유노코딩/3 JS/3로또번호 추첨하기/3script.js
+++ (HTML,CSS,JS)/[HTML,CSS,JS] 유노코딩/3 JS/3로또번호 추첨하기/3script.js
@@ -1,5 +1,5 @@
 //1// .classList //////////////////////////////////////////////////////////////
-
+/*
 const h1 = document.querySelector('h1')
 const addBtn = document.getElementById('add')
 const removeBtn = document.getElementById('remove')
@@ -18,5 +18,66 @@ removeBtn.addEventListener('click',function(){
 toggleBtn.addEventListener('click',function(){
   h1.classList.toggle('text')
 })
+*/
 
 
+//2 배열//////////////////////////////////////////////////////////////////
+
+/*
+const odd = [1,3,5,7,9]
+const even = new Array(2,4,6,8,10)
+
+console.log(odd)
+console.log(even)
+*/
+ 
+/*
+const num = ["영","일","이","삼","사"]
+console.log(num)
+
+let ind = 2;
+console.log(num[ind])
+
+num[ind] = "둘둘22"
+console.log(num[ind])
+*/
+
+//3 배열 메소드/////////////////////////////////////////////////////
+
+/*
+const pet = ["강아지","고양이","앵무새"]
+console.log(pet.length)
+
+console.log(pet)
+pet.push("햄스터","거북이")
+console.log(pet)
+
+pet.pop()
+console.log(pet)
+*/
+
+/*
+const pet = ["강아지","고양이","앵무새"]
+console.log(pet.indexOf("앵무새"))
+console.log(pet.indexOf("없으면-1"))
+*/
+
+/*
+const pet = ["강아지","고양이","앵무새"]
+console.log(pet)
+// pet.splice(1) //index1부터다지워
+// pet.splice(1,2) //1번째부터 2개지워
+// pet.splice(1,1,"다람쥐") //1번째부터 1개지워. 대체데이터넣어
+console.log(pet)
+*/
+
+const pet = ["강아지","고양이","앵무새"]
+console.log(pet)
+
+pet.forEach(function(){
+  console.log("매개변수가 없으면 배열요소 수만큼 호출된다")
+})
+
+pet.forEach(function(param){
+  console.log(param)
+})
