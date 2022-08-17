67a3281eb937ef42ce4e4c8ba3420c63679857f0
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-08-17 10:59:45 +0900

    Create random.java

---
+++ (Java)/JavaJungsuk/src/ch00/random.java
@@ -0,0 +1,23 @@
+package ch00;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class random {
+
+	public static void main(String[] args) {
+
+		Set<Integer> set = new HashSet<Integer>();
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println(">");
+		int count = scan.nextInt();
+
+		while (set.size() < 10) {
+			int num = (int) (Math.random() * count + 1);
+			set.add(num);
+		}
+		System.out.println(set);
+	}
+}
