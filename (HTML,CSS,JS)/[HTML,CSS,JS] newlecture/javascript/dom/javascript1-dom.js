302eb1252812a2710d56706c362976533f5ba407
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-09-19 10:10:42 +0900

    js

--- (HTML,CSS,JS)/[HTML,CSS,JS] newlecture/javascript/dom/javascript1-dom.js
+++ (HTML,CSS,JS)/[HTML,CSS,JS] newlecture/javascript/dom/javascript1-dom.js
@@ -1,5 +1,60 @@
 
-// Ex3 : Ex3 : Selectors API Level1 
+//Ex6 : 메뉴추가(createTextNode, Element)
+window.addEventListener("load", function(){
+  var section = document.querySelector("#section6");
+  
+  var titleInput =section.querySelector(".title-input"); 
+  var menuListDiv =section.querySelector(".menu-list"); 
+  var addButton = section.querySelector(".add-button");
+  var delButton = section.querySelector(".del-button");
+
+  addButton.onclick = function(){
+      var title = titleInput.value;
+      var txtNode = document.createTextNode(title);
+      menuListDiv.appendChild(txtNode);
+  };
+
+  delButton.onclick = function(){
+     
+
+  };
+
+});
+
+// Ex5 : 엘리먼트 노드의 속성 & CSS 속성 변경
+window.addEventListener("load",function () {
+  var section = document.querySelector("#section5");
+  var srcInput = section.querySelector(".src-input");
+  var imgSelect = section.querySelector(".img-select");
+  var colorInput = section.querySelector(".color-input");
+  var changeButton = section.querySelector(".change-button");
+  var img = section.querySelector(".img");
+
+  changeButton.onclick = function() {
+    img.src = "images/"+srcInput.value;
+    // img.src = "images/"+imgSelect.value;
+
+    // img.style["border-color"] = colorInput.value;
+    img.style.borderColor = colorInput.value;
+
+    console.log(srcInput["class"]);
+  };
+});
+
+// Ex4 : Selectors API Level1 
+window.addEventListener("load",function () {
+  var section4 = document.querySelector("#section4");
+  var box = section4.querySelector(".box");
+
+  var input1 = box.children[0]; // 내려쓰면 빈공백이 생기는데 그 빈공백이 노드로 잡혔구나
+  var input2 = box.children[1];
+
+  input1.value="hello";
+  input2.value="okay";
+
+});
+
+// Ex3 : Selectors API Level1 
 window.addEventListener("load",function () {
   var section3 = document.getElementById("section3");
   var txtX = section3.querySelector("input[name='x']"); //속성으로 찾았다
