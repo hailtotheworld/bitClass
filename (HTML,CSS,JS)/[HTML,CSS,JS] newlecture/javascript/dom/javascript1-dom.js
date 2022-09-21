7cbd34fd4bafd7cdcd67a35e061d7375f1427589
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-09-21 18:51:44 +0900

    js

--- (HTML,CSS,JS)/[HTML,CSS,JS] newlecture/javascript/dom/javascript1-dom.js
+++ (HTML,CSS,JS)/[HTML,CSS,JS] newlecture/javascript/dom/javascript1-dom.js
@@ -1,4 +1,50 @@
 
+//Ex10-클릭한 컬럼을 기준으로 레코드 정렬하기 #1
+window.addEventListener("load", function () {
+
+  var notices = [
+    { "id": 1, "title": "유투브에 끌려다니지 않으려고 했는데....ㅜㅜ..", "regDate": "2019-02-05", "writerId": "newlec", "hit": 2 },
+    { "id": 2, "title": "자바스크립트란..", "regDate": "2019-02-02", "writerId": "newlec", "hit": 0 },
+    { "id": 3, "title": "기본기가 튼튼해야....", "regDate": "2019-02-01", "writerId": "newlec", "hit": 1 },
+    { "id": 4, "title": "근데 조회수가 ㅜㅜ..", "regDate": "2019-01-25", "writerId": "newlec", "hit": 0 }
+  ];
+
+  var section = document.querySelector("#section10");
+
+  var noticeList = section.querySelector(".notice-list");
+  var titldTd = section.querySelector(".title");
+  var tbodyNode = noticeList.querySelector("tbody");
+
+  var bindData = function () {
+    var template = section.querySelector("template");
+
+    for (var i = 0; i < notices.length; i++) {
+      var cloneNode = document.importNode(template.content, true);
+      var tds = cloneNode.querySelectorAll("td");
+      tds[0].textContent = notices[i].id;
+
+      var aNode = tds[1].children[0];
+      aNode.href = notices[i].id;
+      aNode.textContent = notices[i].title;
+
+      tds[2].textContent = notices[i].regDate;
+      tds[3].textContent = notices[i].writerId;
+      tds[4].textContent = notices[i].hit;
+
+      tbodyNode.appendChild(cloneNode);
+    }
+  };
+
+  bindData();
+
+  var titleSorted = false;
+
+  titldTd.onclick = function () {
+    // 이 부분에 새로 배운 내용을 작성해 보세요.
+
+  };
+});
+
 //Ex9-다중 노드선택 방법과 일괄삭제, 노드의 자리바꾸기
 window.addEventListener("load", function () {
 
@@ -27,15 +73,27 @@ window.addEventListener("load", function () {
     // 하나하나 돌면서 체크된거 확인하지 말고, 체크된것들만 가져와. (체크된거확인해서가져오기)
     var checkedInputs = tbody.querySelectorAll("input[type='checkbox']:checked");
 
-    for(var i=0;i<checkedInputs.length;i++) {
+    for (var i = 0; i < checkedInputs.length; i++) {
       checkedInputs[i].parentElement.parentElement.remove();
     }
-
   };
 
   swapButton.onclick = function () {
+    var checkedInputs = tbody.querySelectorAll("input[type='checkbox']:checked");
 
+    if (checkedInputs.length != 2) {
+      alert('엘리먼트는 2개를 선택해야만 합니다.')
+      return;
+    }
 
+    var trs = [];
+    for (var i = 0; i < checkedInputs.length; i++) {
+      trs.push(checkedInputs[i].parentElement.parentElement);
+    }
+
+    var cloneNode = trs[0].cloneNode(true);
+    trs[1].replaceWith(cloneNode);
+    trs[0].replaceWith(trs[1]);
   };
 
 });
