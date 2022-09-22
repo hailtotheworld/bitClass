8cecc5c2e8122bd8b93960cf727e1d5f209fa8fb
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-09-22 21:39:22 +0900

    js

--- (HTML,CSS,JS)/[HTML,CSS,JS] newlecture/javascript/javascript2-event.js
+++ (HTML,CSS,JS)/[HTML,CSS,JS] newlecture/javascript/javascript2-event.js
@@ -1,9 +1,35 @@
 
-//연습문제 1-선택된 레코드 삭제하기:event target
-window.addEventListener("load", function(){
+//Ex2-이벤트 버블링을 이용해 사용자 이벤트 처리하기:event Bubbling
+window.addEventListener("load", function () {
 
+  var section = document.querySelector("#section2");
+  var imgList = section.querySelector(".img-list");
+  var currentImg = section.querySelector(".current-img");
+
+  imgList.onclick = function (e) {
+    if(e.target.nodeName != 'IMG') return;
+
+    currentImg.src = e.target.src;
+
+  };
+});
+
+//연습문제 1-선택된 레코드 삭제하기:event target
+window.addEventListener("load", function () {
+
+  var section = document.querySelector('#section1-1');
+  var tbodyNode = section.querySelector('tbody');
+
+  var delButton = tbodyNode.querySelectorAll('.del-button');
+
+  tbodyNode.addEventListener('click',function(e) {
+    if(e.target.nodeName != 'INPUT') return;
+
+    e.target.parentElement.parentElement.remove();
     
-}); 
+  });
+
+});
 
 //Ex1-선택된 이미지 보여주기:event target
 window.addEventListener("load", function () {
@@ -13,7 +39,7 @@ window.addEventListener("load", function () {
   var imgs = section.querySelectorAll(".img");
   var currentImg = section.querySelector(".current-img");
 
-  for(var i=0;i<imgs.length;i++) {
+  for (var i = 0; i < imgs.length; i++) {
     imgs[i].onclick = function (e) {
       console.log(e.target.nodeName); //Node.nodeName은 자신의 대문자 태그명이다.
       currentImg.src = e.target.src;
