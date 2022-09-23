340a73772663891f56e204c46270b4357ae34f1f
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-09-23 18:15:21 +0900

    js

--- (HTML,CSS,JS)/[HTML,CSS,JS] newlecture/javascript/dom_study/javascript1-dom-study.js
+++ (HTML,CSS,JS)/[HTML,CSS,JS] newlecture/javascript/dom_study/javascript1-dom-study.js
@@ -1,80 +1,99 @@
 //Ex10-클릭한 컬럼을 기준으로 레코드 정렬하기 #1
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
 
   var notices = [
-      {"id":1, "title":"유투브에 끌려다니지 않으려고 했는데....ㅜㅜ..", "regDate":"2019-02-05", "writerId":"newlec", "hit":2},
-      {"id":2, "title":"자바스크립트란..", "regDate":"2019-02-02", "writerId":"newlec", "hit":0},
-      {"id":3, "title":"기본기가 튼튼해야....", "regDate":"2019-02-01", "writerId":"newlec", "hit":1},
-      {"id":4, "title":"근데 조회수가 ㅜㅜ..", "regDate":"2019-01-25", "writerId":"newlec", "hit":0}
+    { "id": 1, "title": "c유투브에 끌려다니지 않으려고 했는데....ㅜㅜ..", "regDate": "2019-02-05", "writerId": "newlec", "hit": 2 },
+    { "id": 2, "title": "b자바스크립트란..", "regDate": "2019-02-02", "writerId": "newlec", "hit": 0 },
+    { "id": 3, "title": "z기본기가 튼튼해야....", "regDate": "2019-02-01", "writerId": "newlec", "hit": 1 },
+    { "id": 4, "title": "a근데 조회수가 ㅜㅜ..", "regDate": "2019-01-25", "writerId": "newlec", "hit": 0 }
   ];
 
   var section = document.querySelector("#section10");
-  
-  var noticeList =section.querySelector(".notice-list");
+
+  var noticeList = section.querySelector(".notice-list");
   var titldTd = section.querySelector(".title");
   var tbodyNode = noticeList.querySelector("tbody");
 
-  var bindData = function(){
-      var template = section.querySelector("template");
+  var bindData = function () {
+    var template = section.querySelector("template");
 
-      for(var i=0; i<notices.length; i++){
-          var cloneNode = document.importNode(template.content, true);
-          var tds = cloneNode.querySelectorAll("td");
-          tds[0].textContent = notices[i].id;            
+    for (var i = 0; i < notices.length; i++) {
+      var cloneNode = document.importNode(template.content, true);
+      var tds = cloneNode.querySelectorAll("td");
+      tds[0].textContent = notices[i].id;
 
-          var aNode = tds[1].children[0];
-          aNode.href=notices[i].id;
-          aNode.textContent = notices[i].title;
+      var aNode = tds[1].children[0];
+      aNode.href = notices[i].id;
+      aNode.textContent = notices[i].title;
 
-          tds[2].textContent = notices[i].regDate;
-          tds[3].textContent = notices[i].writerId;
-          tds[4].textContent = notices[i].hit;
+      tds[2].textContent = notices[i].regDate;
+      tds[3].textContent = notices[i].writerId;
+      tds[4].textContent = notices[i].hit;
 
-          tbodyNode.appendChild(cloneNode);
-      }
+      tbodyNode.appendChild(cloneNode);
+    }
   };
 
   bindData();
 
   var titleSorted = false;
 
-  titldTd.onclick = function(){
-      // 이 부분에 새로 배운 내용을 작성해 보세요.
-     
+  titldTd.onclick = function () {
+
+    tbodyNode.innerHTML = "";
+
+    if (!titleSorted) {
+      notices.sort(function (a, b) {
+        if (a.title > b.title) {
+          return 1;
+        }
+        if (a.title < b.title) {
+          return -1;
+        }
+        // a must be equal to b
+        return 0;
+      });
+      titleSorted=true;
+    }
+    
+    bindData();
+    notices.reverse();
+
+
   };
 });
 
 //Ex9-다중 노드선택 방법과 일괄삭제, 노드의 자리바꾸기
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
 
   var section = document.querySelector("#section9");
-  
-  var noticeList =section.querySelector(".notice-list"); 
+
+  var noticeList = section.querySelector(".notice-list");
   var tbody = noticeList.querySelector("tbody");
   var allCheckbox = section.querySelector(".overall-checkbox");
   var delButton = section.querySelector(".del-button");
   var swapButton = section.querySelector(".swap-button");
 
-  allCheckbox.onchange = function(){
+  allCheckbox.onchange = function () {
 
     var checkedInputs = tbody.querySelectorAll('input[type="checkbox"]');
 
-      for(var i=0;i<checkedInputs.length;i++) {
-        checkedInputs[i].checked = allCheckbox.checked;
-      }
+    for (var i = 0; i < checkedInputs.length; i++) {
+      checkedInputs[i].checked = allCheckbox.checked;
+    }
 
   };
 
-  delButton.onclick = function(){
+  delButton.onclick = function () {
 
     var checkedInputs = tbody.querySelectorAll('input[type="checkbox"]:checked');
-    
-    for(var i=0;i<checkedInputs.length;i++)
-    checkedInputs[i].parentElement.parentElement.remove();
+
+    for (var i = 0; i < checkedInputs.length; i++)
+      checkedInputs[i].parentElement.parentElement.remove();
 
   };
 
-  swapButton.onclick = function(){
+  swapButton.onclick = function () {
 
     //복사본을만든다
     //복사본이1번과교대
@@ -82,135 +101,135 @@ window.addEventListener("load", function(){
 
     var checkedInputs = tbody.querySelectorAll('input[type="checkbox"]:checked');
 
-    if(checkedInputs.length!=2) {
+    if (checkedInputs.length != 2) {
       alert('2개만 고르세요.')
     }
 
     var trs = new Array(checkedInputs.length);
-    for(var i=0;i<checkedInputs.length;i++) {
+    for (var i = 0; i < checkedInputs.length; i++) {
       trs[i] = checkedInputs[i].parentElement.parentElement;
     }
 
     var clone = trs[0].cloneNode(true);
     trs[1].replaceWith(clone);
     trs[0].replaceWith(trs[1]);
-              
-      
+
+
   };
 
 });
 
 //Ex8-노드 삽입과 바꾸기
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
 
   var section = document.querySelector("#section8");
-  
-  var noticeList =section.querySelector(".notice-list"); 
+
+  var noticeList = section.querySelector(".notice-list");
   var tbodyNode = noticeList.querySelector("tbody");
   var upButton = section.querySelector(".up-button");
   var downButton = section.querySelector(".down-button");
 
   var currentNode = tbodyNode.firstElementChild;//.children[0];
 
-  downButton.onclick = function(){
+  downButton.onclick = function () {
     var nextNode = currentNode.nextElementSibling;
-  
-    if(nextNode==null)
+
+    if (nextNode == null)
       alert('이동할 수 없어요')
 
-    currentNode.insertAdjacentElement('beforebegin',nextNode);
-      
+    currentNode.insertAdjacentElement('beforebegin', nextNode);
+
 
   };
 
-  upButton.onclick = function(){
+  upButton.onclick = function () {
     var prevNode = currentNode.previousElementSibling;
 
-    if(prevNode==null)
+    if (prevNode == null)
       alert('이동할수없습니다')
 
-    currentNode.insertAdjacentElement('afterend',prevNode);
-    
+    currentNode.insertAdjacentElement('afterend', prevNode);
+
   };
 
 });
 
 
 //Ex7 : 노드 복제와 템플릿 태그
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
   var notices = [
-      {id:5, title:"퐈이야~~~", regDate:"2019-01-26", writerId:"newlec", hit:0},
-      {id:6, title:"나 좀 복제해줘~", regDate:"2019-01-26", writerId:"newlec", hit:17}
+    { id: 5, title: "퐈이야~~~", regDate: "2019-01-26", writerId: "newlec", hit: 0 },
+    { id: 6, title: "나 좀 복제해줘~", regDate: "2019-01-26", writerId: "newlec", hit: 17 }
   ];
 
   var section = document.querySelector("#section7");
-  
-  var noticeList =section.querySelector(".notice-list"); 
+
+  var noticeList = section.querySelector(".notice-list");
   var tbodyNode = noticeList.querySelector("tbody");
   var cloneButton = section.querySelector(".clone-button");
   var templateButton = section.querySelector(".template-button");
 
-  cloneButton.onclick = function(){
+  cloneButton.onclick = function () {
     var tr = tbodyNode.querySelectorAll('tr');
     var clone = tr[0].cloneNode(true);
     var cloneTds = clone.querySelectorAll('td');
     cloneTds[0].innerHTML = notices[0].id;
-    cloneTds[1].innerHTML = '<a href='+notices[0].id+'>'+notices[0].title+'</a>';
+    cloneTds[1].innerHTML = '<a href=' + notices[0].id + '>' + notices[0].title + '</a>';
     cloneTds[2].innerHTML = notices[0].regDate;
     cloneTds[3].innerHTML = notices[0].writerId;
     cloneTds[4].innerHTML = notices[0].hit;
 
     tbodyNode.appendChild(clone);
-      
+
   };
 
-  templateButton.onclick = function(){
+  templateButton.onclick = function () {
     var template = section.querySelector('template');
-    var clone = document.importNode(template.content,true);
+    var clone = document.importNode(template.content, true);
 
     var cloneTds = clone.querySelectorAll('td');
     cloneTds[0].innerHTML = notices[0].id;
-    cloneTds[1].innerHTML = '<a href='+notices[0].id+'>'+notices[0].title+'</a>';
+    cloneTds[1].innerHTML = '<a href=' + notices[0].id + '>' + notices[0].title + '</a>';
     cloneTds[2].innerHTML = notices[0].regDate;
     cloneTds[3].innerHTML = notices[0].writerId;
     cloneTds[4].innerHTML = notices[0].hit;
 
     tbodyNode.appendChild(clone);
-      
-      
+
+
   };
 
 });
 
 
 //Ex6 : 메뉴추가(createTextNode, Element)
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
   var section = document.querySelector("#section6");
-  
-  var titleInput =section.querySelector(".title-input"); 
-  var menuListUl =section.querySelector(".menu-list"); 
+
+  var titleInput = section.querySelector(".title-input");
+  var menuListUl = section.querySelector(".menu-list");
   var addButton = section.querySelector(".add-button");
   var delButton = section.querySelector(".del-button");
 
-  addButton.onclick = function(){
+  addButton.onclick = function () {
     var li = document.createElement('li');
-    li.innerHTML = '<a href="">'+titleInput.value+'</a>';
+    li.innerHTML = '<a href="">' + titleInput.value + '</a>';
 
     menuListUl.appendChild(li);
-    titleInput.value="";
+    titleInput.value = "";
   };
 
-  delButton.onclick = function(){
+  delButton.onclick = function () {
     menuListUl.firstElementChild.remove();
     // menuListUl.lastElementChild.remove();
-     
+
 
   };
 
 });
 
 //Ex5 : 엘리먼트 노드의 속성& CSS 속성 변경
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
   var section = document.querySelector("#section5");
   var srcInput = section.querySelector(".src-input");
   var imgSelect = section.querySelector(".img-select");
@@ -218,14 +237,14 @@ window.addEventListener("load", function(){
   var img = section.querySelector(".img");
   var colorInput = section.querySelector(".color-input");
 
-  changeButton.onclick = function(){
-      
+  changeButton.onclick = function () {
+
   };
 
 });
 
 //Ex4 : childeNodes를 이용한 노드 선택
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
   var section4 = document.querySelector("#section4");
   var box = section4.querySelector(".box");
 
@@ -238,20 +257,20 @@ window.addEventListener("load", function(){
 });
 
 //Ex3 : Selectors API Level1
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
   var section3 = document.getElementById("section3");
   var txtX = section3.querySelector("input[name='x']");
   var txtY = section3.querySelector(".txt-y");
   var btnAdd = section3.querySelector(".btn-add");
   var txtSum = section3.querySelector(".txt-sum");
 
-  btnAdd.onclick = function () {               
-       
+  btnAdd.onclick = function () {
+
   };
 });
 
 //Ex2 : 엘리먼트 선택방법 개선하기
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
   var section2 = document.getElementById("section2");
   var txtX = section2.getElementsByClassName("txt-x")[0];
   var txtY = section2.getElementsByClassName("txt-y")[0];
@@ -266,19 +285,19 @@ window.addEventListener("load", function(){
   var txtSum = inputs[3];
   */
 
-  btnAdd.onclick = function () {               
-       
+  btnAdd.onclick = function () {
+
   };
 });
 
 //Ex1 : 계산기 프로그램
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
   var txtX = document.getElementById("txt-x");
   var txtY = document.getElementById("txt-y");
   var btnAdd = document.getElementById("btn-add");
   var txtSum = document.getElementById("txt-sum");
- 
-  btnAdd.onclick = function () {               
-       
+
+  btnAdd.onclick = function () {
+
   };
 });
--- (HTML,CSS,JS)/[HTML,CSS,JS] newlecture/javascript/javascript2-event.js
+++ (HTML,CSS,JS)/[HTML,CSS,JS] newlecture/javascript/javascript2-event.js
@@ -1,4 +1,90 @@
 
+// Ex5-엘리먼트 노드의 기본행위 막기
+window.addEventListener("load", function () {
+
+  var section = document.querySelector("#section5");
+  var tbody = section.querySelector(".notice-list tbody");
+
+  tbody.onclick = function (e) {
+    e.preventDefault();
+
+    var target = e.target;
+
+    if (target.nodeName != "A") return;
+
+    if (target.classList.contains = "sel-button") {
+
+      var tr = target.parentElement;
+      for (; tr.nodeName != "TR"; tr = tr.parentElement);
+
+      tr.style.background = "yellow";
+
+    } else if (target.classList.contains = "edit-button") {
+
+    } else if (target.classList.contains = "del-button") {
+
+    }
+  }
+});
+
+// Ex4-서로 다른 기능의 여러 버튼을 가진 화면에서 이벤트를 처리하는 방법
+window.addEventListener("load", function () {
+
+  var section = document.querySelector("#section4");
+  var tbody = section.querySelector(".notice-list tbody");
+
+  tbody.onclick = function (e) {
+    var target = e.target;
+
+    if (target.nodeName != "INPUT") return;
+
+    //if(target.className = "sel-button") //클래스가 1개라면 사용할수있긴한데, 클래스이름이 단독이름을 갖고있는 경우는 많지가 않다.
+    if (target.classList.contains = "sel-button") {
+
+      // var tr = target.parentElement.parentElement; //이렇게 찾는건 위험이 있다..!
+      var tr = target.parentElement;
+      for (; tr.nodeName != "TR"; tr = tr.parentElement);
+
+      tr.style.background = "yellow";
+
+    } else if (target.classList.contains = "edit-button") {
+
+    } else if (target.classList.contains = "del-button") {
+
+    }
+  }
+});
+
+// Ex3-이벤트 버블링 멈추기
+window.addEventListener("load", function () {
+
+  var section = document.querySelector("#section3");
+
+  var imgList = section.querySelector(".img-list");
+  var addButton = section.querySelector(".add-button");
+  var currentImg = section.querySelector(".current-img");
+
+  imgList.onclick = function (e) {
+    console.log('imgList.onclick');
+    if (e.target.nodeName != 'IMG') return;
+
+    currentImg.src = e.target.src;
+
+  };
+
+  addButton.onclick = function (e) {
+    e.stopPropagation(); // 부모에게 이벤트 전파되는걸 막는 메서드
+
+    console.log('addButton.onclick');
+    var img = document.createElement('img');
+    img.src = "images/img1.jpg";
+    currentImg.insertAdjacentElement('afterend', img);
+
+
+  };
+
+});
+
 //Ex2-이벤트 버블링을 이용해 사용자 이벤트 처리하기:event Bubbling
 window.addEventListener("load", function () {
 
@@ -7,7 +93,7 @@ window.addEventListener("load", function () {
   var currentImg = section.querySelector(".current-img");
 
   imgList.onclick = function (e) {
-    if(e.target.nodeName != 'IMG') return;
+    if (e.target.nodeName != 'IMG') return;
 
     currentImg.src = e.target.src;
 
@@ -22,11 +108,11 @@ window.addEventListener("load", function () {
 
   var delButton = tbodyNode.querySelectorAll('.del-button');
 
-  tbodyNode.addEventListener('click',function(e) {
-    if(e.target.nodeName != 'INPUT') return;
+  tbodyNode.addEventListener('click', function (e) {
+    if (e.target.nodeName != 'INPUT') return;
 
     e.target.parentElement.parentElement.remove();
-    
+
   });
 
 });
