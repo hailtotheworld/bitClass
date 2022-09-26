b3dbb0f32d25fae2128df59451e9b2be59e2c26a
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-09-26 18:10:35 +0900

    js

--- (HTML,CSS,JS)/[HTML,CSS,JS] newlecture/javascript/javascript3-event.js
+++ (HTML,CSS,JS)/[HTML,CSS,JS] newlecture/javascript/javascript3-event.js
@@ -1,4 +1,36 @@
 
+// Ex7-마우스 이벤트 객체: 드래그 방식으로 박스 옮기기
+window.addEventListener("load", function() {
+  var section = document.querySelector('#section7');
+  var container = section.querySelector(".container");
+  var box = section.querySelector(".box");
+
+  var dragging = false;
+  var offset = {x:0,y:0};
+
+  container.onmousedown = function(e) {
+    if(e.target === box)
+        dragging = true;
+  };
+
+  container.onmousemove = function(e) {
+    if(!dragging) return;
+
+    box.style.left = e.pageX-offset.x+"px";
+    box.style.top = e.pageY-offset.y+"px";
+  };
+
+  container.onmouseup = function(e) {
+    dragging = false;
+  };
+
+  box.onmousedown = function(e) {
+    offset.x = e.offsetX;
+    offset.y = e.offsetY;
+  };
+
+});
+
 // Ex6-MouseEvent Position
 window.addEventListener("load", function() {
   var section = document.querySelector('#section6');
@@ -6,11 +38,18 @@ window.addEventListener("load", function() {
   var box = section.querySelector(".box");
 
   container.onclick = function(e) {
-    // e.x, e.y / e.offsetX, e.offsetY / e.clientX, e.pageX ...
     console.log("(x,y):"+e.x+","+e.y);
-    box.style.left = e.x+"px";
-    box.style.top = e.y+"px";
+    console.log("client (x,y):"+e.clientX+","+e.clientY);
+    console.log("page (x,y):"+e.pageX+","+e.pageY);
+    console.log("offset (x,y):"+e.offsetX+","+e.offsetY);
+    // box.style.left = e.pageX+"px";
+    // box.style.top = e.pageY+"px";
 
+    // box.style.left = e.x+"px"; // clientX
+    // box.style.top = e.y+"px";
+
+    box.style.left = e.offsetX+"px";
+    box.style.top = e.offsetY+"px";
   }
-})
+});
 
