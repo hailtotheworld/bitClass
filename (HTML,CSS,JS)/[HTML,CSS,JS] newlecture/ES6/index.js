0c1a802c66d7903ca7766487d2a531a129902c39
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-09-27 09:31:30 +0900

    js

--- (HTML,CSS,JS)/[HTML,CSS,JS] newlecture/ES6/index.js
+++ (HTML,CSS,JS)/[HTML,CSS,JS] newlecture/ES6/index.js
@@ -1,5 +1,5 @@
 
-//let//////////////////////////////////////////////////////////////////////////////////////
+//1. let은 재선언이 불가능하다//////////////////////////////////////////////////////////////////////////////////////
 /*
 var x = 30;
 console.log(x);
@@ -14,9 +14,36 @@ console.log(window.y); // let으로 선언한건 window객체의 전역속성이
 // let y = 51; // let으로는 재선언이 불가능
 */
 
-//let//////////////////////////////////////////////////////////////////////////////////////
+//2. let을 통핸 변수의 지역화//////////////////////////////////////////////////////////////////////////////////////
+/*
 {var a = 30;}
 console.log(a);
 
 {let b = 50;} // let은 괄호블록을 사용하면 지역변수로 사용된다
 // console.log(b); //에러
+
+function getValue(contidition) {
+  if(contidition){
+    var value = "ok";
+    return value;
+  } else{
+    return value;
+  }
+}
+
+console.log(getValue(true));
+console.log(getValue(false));//undefined: 변수가 선언되어있다는걸 의미한다. 값대입이 안되어있다는의미. 에러가나야정상인데...
+
+
+function getLetValue(contidition) {
+  if(contidition){ 
+    let value = "ok";
+    return value;
+  } else{
+    return value;
+  }
+}
+
+console.log(getLetValue(true));
+console.log(getLetValue(false));//에러발생. 이게 정상이지. let변수의 생명주기가 {}까지니까.
+*/
