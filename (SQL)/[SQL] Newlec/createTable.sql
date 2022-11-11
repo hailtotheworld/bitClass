0cf92d709c8b9f745caf285881ceabbd039ecc53
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-11-11 22:07:21 +0900

    sql

--- (SQL)/[SQL] Newlec/createTable.sql
+++ (SQL)/[SQL] Newlec/createTable.sql
@@ -1,10 +1,26 @@
 CREATE TABLE MEMBER (
-    ID  VARCHAR2(50),
-    PWD VARCHAR2(50),
-    NAME VARCHAR2(50),
-    GENDER VARCHAR2(50),
-    AGE NUMBER,
-    BIRTHDAY VARCHAR2(50),
-    PHONE VARCHAR2(50),
-    REGDATE DATE
+    ID       VARCHAR2(50),
+    PWD      NVARCHAR2(50),
+    NAME     NVARCHAR2(50),
+--  GENDER   CHAR(6),      --1글자3바이트
+--  GENDER   CHAR(2 CHAR), --1글자3바이트
+    GENDER   NCHAR(2),     --1글자2바이트
+    AGE      NUMBER,
+    BIRTHDAY CHAR(10), --1990-10-29
+    PHONE    CHAR(13), --010-1234-5678
+    REGDATE  DATE
 );
+
+drop table member;
+
+INSERT into member(gender) VALUES('남성');
+
+select * from member;
+
+select length('ab') from dual;
+select lengthb('ab') from dual; --lengthb는 몇바이트 쓰는지 알려준다.
+
+select length('한글') from dual; --2
+select lengthb('한글') from dual; --6 한글은 한글자에 3바이트씩 차지한다.
+
+select * from nls_database_parameters;
