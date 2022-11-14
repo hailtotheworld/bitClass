c4816f0b8fa7732af625b85c1064ba1870e821a1
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-11-14 20:05:26 +0900

    SQL

--- (SQL)/[SQL] Newlec/20rownum.sql
+++ (SQL)/[SQL] Newlec/20rownum.sql
@@ -3,6 +3,8 @@ SELECT * from notice;
 
 SELECT * from notice where rownum BETWEEN 1 and 5;
 
+SELECT * from notice where rownum <= 5;
+
 SELECT * from notice where rownum BETWEEN 6 and 10; --안됨
 
 select * from (select ROWNUM num, notice.* from notice) where num between 6 and 12;
