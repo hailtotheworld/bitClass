243f9c70ab233db2e953c60de28c0081a99a89ec
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-11-17 11:11:42 +0900

    sql

--- (SQL)/[SQL] Newlec/34~38JOIN.sql
+++ (SQL)/[SQL] Newlec/34~38JOIN.sql
@@ -2,5 +2,6 @@ select rownum, member.*
 from (select rownum N, member.* from member order by name)
 where N between 1 and 10;
 
+select * from member;
 
-select
+select phone, name from member group by phone, name;
