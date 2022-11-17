1856cbc8f18efa2283386db8f3f3ec8bfdf220f4
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-11-17 12:03:27 +0900

    sql

--- (SQL)/[SQL] Newlec/34~38JOIN.sql
+++ (SQL)/[SQL] Newlec/34~38JOIN.sql
@@ -5,3 +5,7 @@ where N between 1 and 10;
 select * from member;
 
 select phone, name from member group by phone, name;
+
+select id,name from member
+union
+select notice.writer_id, notice.title from notice;
---
+++ (SQL)/[SQL] Newlec/41데이터딕셔너리.sql
@@ -0,0 +1,9 @@
+
+
+select * from DICT;
+
+select * from user_tables;
+
+select * from user_tab_columns;
+
+select * from user_tab_columns where TABLE_NAME = 'MEMBER';
