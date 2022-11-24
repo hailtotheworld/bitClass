0376d00a2cfc0f4978d4332e4194e5a2b6734e40
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-11-24 19:12:49 +0900

    jdbc

--- (Java)/JDBCPrjClone/src/com/newlecture/app/console/NoticeConsoleCl.java
+++ (Java)/JDBCPrjClone/src/com/newlecture/app/console/NoticeConsoleCl.java
@@ -2,6 +2,7 @@ package com.newlecture.app.console;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 import com.newlecture.app.entity.NoticeCl;
 import com.newlecture.app.service.NoticeServiceCl;
@@ -33,8 +34,15 @@ public class NoticeConsoleCl {
 	}
 
 	public int inputNoticeMenu() {
-		// TODO Auto-generated method stub
-		return 0;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("1.상세조회 / 2.이전 / 3.다음 / 4.글쓰기 / 5.종료 >");
+		
+		
+		String menu_ = scan.nextLine();
+		int menu = Integer.parseInt(menu_);
+		
+		
+		return menu;
 	}
 
 }
--- (Java)/JDBCPrjClone/src/program/ProgramCl.java
+++ (Java)/JDBCPrjClone/src/program/ProgramCl.java
@@ -9,20 +9,33 @@ public class ProgramCl {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
 		NoticeConsoleCl console = new NoticeConsoleCl();
-		
-		console.printNoticeList();
-		int menu = console.inputNoticeMenu();
-		
-		switch(menu) {
-		case 1: // 상세조회
-			break;
-		case 2: // 이전
-			break;
-		case 3: // 다음
-			break;
-		case 4: // 글쓰기
-			break;
+
+		EXIT:
+		while (true) {
+			console.printNoticeList();
+			int menu = console.inputNoticeMenu();
+
+			switch (menu) {
+			case 1: // 상세조회
+				break;
+			case 2: // 이전
+				break;
+			case 3: // 다음
+				break;
+			case 4: // 글쓰기
+				break;
+			case 5: // 종료
+				System.out.println("bye~~");
+				break EXIT;
+			default:
+				System.out.println("1~5까지만 입력할 수 있습니다.");
+				break;
+			}
 		}
+		
+
+		
+		
 	}
 
 }
