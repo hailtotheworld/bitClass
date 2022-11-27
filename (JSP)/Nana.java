2542e1ff3d536e9f7de48d761d1fc7e318c0d91a
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-11-27 19:29:40 +0900

    jsp

--- (JSP)/Nana.java
+++ (JSP)/Nana.java
@@ -5,6 +5,9 @@ import java.io.*;
 public class Nana extends HttpServlet {
  public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
  {
-  System.out.println("hello Servlet");
+  // System.out.println("hello Servlet");
+OutputStream os = response.getOutputStream();
+PrintStream out = new PrintStream(os, true);
+out.print("Hello Servlet");
  }
 }
