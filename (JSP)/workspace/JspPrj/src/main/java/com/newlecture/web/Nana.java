8dc2de1f8e5cfdbaeca4a85f73abbc036db64452
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-11-29 19:37:10 +0900

    jsp

--- (JSP)/workspace/JspPrj/src/main/java/com/newlecture/web/Nana.java
+++ (JSP)/workspace/JspPrj/src/main/java/com/newlecture/web/Nana.java
@@ -10,20 +10,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/hi")
-public class Nana extends HttpServlet{
-	
-	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class Nana extends HttpServlet {
 
-		response.setCharacterEncoding("UTF-8"); //서버가 UTF-8로 보내겠다.
-		response.setContentType("text/html; charset=UTF-8"); //클라이언트야 이문서는 HTML문서다. UTF-8로 읽어라.
-		
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setCharacterEncoding("UTF-8"); // 서버가 UTF-8로 보내겠다.
+		response.setContentType("text/html; charset=UTF-8"); // 클라이언트야 이문서는 HTML문서다. UTF-8로 읽어라.
+
 		PrintWriter out = response.getWriter();
 //		out.println("Hello~~ hihi");
+
+//		for(int i=0;i<100;i++) {
+//			out.println((i+1)+": 안녕 Hello Servlet!! <br>");
+//		}
+
+//		int cnt = Integer.parseInt(request.getParameter("cnt"));
+
+		String cnt_ = request.getParameter("cnt");
 		
-		for(int i=0;i<100;i++) {
-			out.println((i+1)+": 안녕 Hello Servlet!! <br>");
+		int cnt = 100;
+		if(cnt_!=null && !cnt_.equals("")) {
+			cnt = Integer.parseInt(cnt_);
 		}
+		
+		for (int i = 0; i < cnt; i++) {
+			out.print((i+1)+": 안녕 Servlet <br>");
+		}
+
 	}
 
 }
