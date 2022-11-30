07dbb4d40954c3d1031f0098cb3a24c7cf654b5a
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-11-30 21:55:14 +0900

    jsp

--- (JSP)/workspace/JspPrj/src/main/java/com/newlecture/web/Calc2.java
+++ (JSP)/workspace/JspPrj/src/main/java/com/newlecture/web/Calc2.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,6 +20,9 @@ public class Calc2 extends HttpServlet {
 
 		ServletContext application = request.getServletContext();
 		HttpSession session = request.getSession();
+
+		// (3)클라이언트에게서 전달된 쿠키를 읽는다.
+		Cookie[] cookies = request.getCookies();
 
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
@@ -38,10 +42,31 @@ public class Calc2 extends HttpServlet {
 		if (op.equals("=")) {
 
 //			int x = (int) application.getAttribute("value");
-			int x = (int) session.getAttribute("value");
+//			int x = (int) session.getAttribute("value");
+
+			// (4) 받은 쿠키들에서 이름이 같은쿠키를 찾아서 값을 대입한다.
+			int x = 0;
+			for (Cookie c : cookies) {
+				if (c.getName().equals("value")) {
+					x = Integer.parseInt(c.getValue());
+					break;
+				}
+			}
+			
+
 			int y = v;
 //			String operator = (String) application.getAttribute("op");
-			String operator = (String) session.getAttribute("op");
+//			String operator = (String) session.getAttribute("op");
+			
+			String operator = "";
+			for(Cookie c : cookies) {
+				if(c.getName().equals("op")) {
+					operator = c.getValue();
+					break;
+				}
+			}
+			
+			
 			int result = 0;
 
 			if (operator.equals("+")) {
@@ -56,9 +81,19 @@ public class Calc2 extends HttpServlet {
 		// 값을 저장
 		else {
 //			application.setAttribute("value", v);
-			session.setAttribute("value", v);
 //			application.setAttribute("op", op);
-			session.setAttribute("op", op);
+
+//			session.setAttribute("value", v);
+//			session.setAttribute("op", op);
+
+			// (1)쿠키를 만든다.
+			Cookie valueCookie = new Cookie("value", String.valueOf(v));
+			Cookie opCookie = new Cookie("op", op);
+
+			// (2)쿠키를 클라이언트에 보낸다
+			response.addCookie(valueCookie);
+			response.addCookie(opCookie);
+
 		}
 
 	}
