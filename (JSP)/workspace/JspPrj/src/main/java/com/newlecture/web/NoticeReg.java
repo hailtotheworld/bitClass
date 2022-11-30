48fc666ea56c01f08e3ac22914a5d4877f5a90f5
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-11-30 12:33:35 +0900

    jsp

--- (JSP)/workspace/JspPrj/src/main/java/com/newlecture/web/NoticeReg.java
+++ (JSP)/workspace/JspPrj/src/main/java/com/newlecture/web/NoticeReg.java
@@ -16,10 +16,13 @@ public class NoticeReg extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+//		request.setCharacterEncoding("UTF-8");
+
 		response.setCharacterEncoding("UTF-8"); // 서버가 UTF-8로 보내겠다.
 		response.setContentType("text/html; charset=UTF-8"); // 클라이언트야 이문서는 HTML문서다. UTF-8로 읽어라.
-		request.setCharacterEncoding("UTF-8");
-
+		//모든 응답에 콘텐츠타입이 css나 image라 할지라도 text/html로 고정되서 위 setContentType메서드는 필터클래스에 넣지 않아야 한다.
+		
+		
 		PrintWriter out = response.getWriter();
 
 		String title = request.getParameter("title");
@@ -27,7 +30,7 @@ public class NoticeReg extends HttpServlet {
 		
 		out.println(title);
 		out.println(content);
-
+		
 	}
 
 }
