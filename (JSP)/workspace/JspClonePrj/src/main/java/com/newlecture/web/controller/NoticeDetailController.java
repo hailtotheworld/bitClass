522fc9902de94a5af8bdd3538f0252e68b822d9c
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-12-06 08:50:20 +0900

    jsp

---
+++ (JSP)/workspace/JspClonePrj/src/main/java/com/newlecture/web/controller/NoticeDetailController.java
@@ -0,0 +1,94 @@
+package com.newlecture.web.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.newlecture.web.entitiy.Notice;
+
+@WebServlet("/notice/detail")
+public class NoticeDetailController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		int id = Integer.parseInt(request.getParameter("id"));
+
+		String url = "jdbc:oracle:thin:@192.168.1.2:1521/xepdb1";
+		String sql = "SELECT * FROM notice where id = ?";
+		String uid = "SCOTT";
+		String pwd = "tiger";
+		Connection con = null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(url, uid, pwd);
+			st = con.prepareStatement(sql);
+			st.setInt(1, id);
+			rs = st.executeQuery();
+
+			rs.next();
+
+			String title = rs.getString("TITLE");
+			String writerId = rs.getString("WRITER_ID");
+			Date regdate = rs.getDate("REGDATE");
+			String content = rs.getString("CONTENT");
+			int hit = rs.getInt("hit");
+			String files = rs.getString("FILES");
+
+			Notice notice = new Notice(id, title, writerId, content, regdate, hit, files);
+			
+			request.setAttribute("n", notice);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/notice/detail.jsp");
+			dispatcher.forward(request, response);
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			if (st != null)
+				try {
+					st.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+		}
+
+
+
+	}
+
+}
---
+++ (JSP)/workspace/JspClonePrj/src/main/java/com/newlecture/web/controller/NoticeListController.java
@@ -0,0 +1,100 @@
+package com.newlecture.web.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.newlecture.web.entitiy.Notice;
+
+
+@WebServlet("/notice/list")
+public class NoticeListController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String url = "jdbc:oracle:thin:@192.168.1.2:1521/xepdb1";
+		String sql = "SELECT * FROM notice";
+		String uid = "SCOTT";
+		String pwd = "tiger";
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		List<Notice> list = new ArrayList<Notice>();
+		
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(url, uid, pwd);
+			st = con.createStatement();
+			rs = st.executeQuery(sql);
+			
+			while(rs.next()) {
+				
+				int id = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				String writerId = rs.getString("WRITER_ID");
+				Date regdate = rs.getDate("REGDATE");
+				String content = rs.getString("CONTENT");
+				int hit = rs.getInt("hit");
+				String files = rs.getString("FILES");
+			
+				Notice notice = new Notice(id, title, writerId, content, regdate, hit, files);
+				
+				list.add(notice);
+			}
+		
+			request.setAttribute("list", list);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/notice/list.jsp");
+			dispatcher.forward(request, response);
+						
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if(rs!=null)
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			if(st!=null)
+				try {
+					st.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			if(con!=null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
+		}
+			
+		
+	}
+
+
+
+}
