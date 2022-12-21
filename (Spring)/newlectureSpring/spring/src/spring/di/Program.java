fd0d08619f12ee8e10f9e22e8ca285b2e3ff4696
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-12-21 17:55:12 +0900

    spring

--- (Spring)/newlectureSpring/spring/src/spring/di/Program.java
+++ (Spring)/newlectureSpring/spring/src/spring/di/Program.java
@@ -24,7 +24,6 @@ public class Program {
 		// Exam 인터페이스, NewlecExam 데이터구현객체
 						
 		// ExamConsole console = new InlineExamConsole(exam); // InlineExamConsole가 exam객체를 조립하고 있다. 이작업이 DI다.
-		
 		// ExamConsole console = new GridExamConsole();
 		// 객체를 생성하는 부분을 비워두고 이 부분을 Spring이 대신 해줄수있도록 설정을 빼고 조립시키는 도움을 받으면,
 		// 소스코드 수정없이 InlineExamConsole,GridExamConsole 바꿔낄수있게된다.
