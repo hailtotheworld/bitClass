9d95e50f558710e44c1de89faf324a6b5d39d4e6
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-12-21 14:27:29 +0900

    spring

--- (Spring)/newlectureSpring/spring/src/spring/di/Program.java
+++ (Spring)/newlectureSpring/spring/src/spring/di/Program.java
@@ -14,7 +14,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		/* 스프링에게 지시하는 방법으로 코드를 변경
+		/* 
 		Exam exam = new NewlecExam();
 		ExamConsole console = new GridExamConsole();
 		console.setExam(exam);
@@ -23,14 +23,16 @@ public class Program {
 		// Exam exam = new NewlecExam();
 		// Exam 인터페이스, NewlecExam 데이터구현객체
 						
-		// ExamConsole console = new InlineExamConsole(exam); // InlineExamConsole객체가 exam객체를 조립하고 있다. 이작업이 DI다.
+		// ExamConsole console = new InlineExamConsole(exam); // InlineExamConsole가 exam객체를 조립하고 있다. 이작업이 DI다.
 		
 		// ExamConsole console = new GridExamConsole();
 		// 객체를 생성하는 부분을 비워두고 이 부분을 Spring이 대신 해줄수있도록 설정을 빼고 조립시키는 도움을 받으면,
 		// 소스코드 수정없이 InlineExamConsole,GridExamConsole 바꿔낄수있게된다.
 		// 바뀌는거라면 정적으로 코드로 박아놓는것보다는 설정으로 빼는게 바람직하다.
 
-		//////
+		
+		
+		////// 스프링에게 지시하는 방법으로 코드를 변경
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
 		// 스프링라이브러리를 받기위해서 Maven프로젝트로 변경해준다.(프로젝트 우클릭-configure-Convert to Maven Project)
