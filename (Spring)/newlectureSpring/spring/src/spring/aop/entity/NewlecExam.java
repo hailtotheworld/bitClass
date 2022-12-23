37007de036badca5febe8a0c076e6757b2dbeb35
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-12-23 22:26:05 +0900

    spring

--- (Spring)/newlectureSpring/spring/src/spring/aop/entity/NewlecExam.java
+++ (Spring)/newlectureSpring/spring/src/spring/aop/entity/NewlecExam.java
@@ -63,7 +63,6 @@ public class NewlecExam implements Exam {
 		try {
 			Thread.sleep(200);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
