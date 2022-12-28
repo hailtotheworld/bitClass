e432c027ea0588f7ad0887a3323d264bb1baf450
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-12-28 20:19:42 +0900

    spring

--- (Spring)/newlecSpringMVC/webprj1228/src/main/java/com/newlecture/web/controller/IndexController.java
+++ (Spring)/newlecSpringMVC/webprj1228/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController implements Controller{
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		ModelAndView mv = new ModelAndView("/WEB-INF/view/index.jsp");
+		ModelAndView mv = new ModelAndView("index");
 		mv.addObject("data", "데이터다");
 		return mv;
 	}
