15d526354e4d5bbfcd98bb37477c7bbb294c5c8a
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2022-12-31 16:34:02 +0900

    spring

--- (Spring)/newlecSpringMVC/webprj/src/main/java/com/newlecture/web/controller/HomeController.java
+++ (Spring)/newlecSpringMVC/webprj/src/main/java/com/newlecture/web/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.newlecture.web.controller;
 
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /*
 
@@ -34,8 +40,21 @@ public class IndexController implements Controller{
 @RequestMapping("/")
 public class HomeController {
 
+//	@RequestMapping("index")
+//	public String index() {
+//		return "root.index";
+//	}
+	
+//	@RequestMapping("index")
+//	public void index(HttpServletResponse response) throws IOException {
+//		PrintWriter out = response.getWriter();
+//		out.println("Hello Index");
+//	}
+	
 	@RequestMapping("index")
-	public String index() {
-		return "root.index";
+	@ResponseBody //@ResponseBody가 있어야지 view로 가지 않고, 문자열자체가 사용자에게 가길 원한다고 알려주는거다.
+	public String index()  {
+		return "Hello Index";
 	}
+	
 }
