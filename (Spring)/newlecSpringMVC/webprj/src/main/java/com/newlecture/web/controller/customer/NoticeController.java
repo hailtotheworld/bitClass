b736d4c1db08c3ba83450e1442534c194db795b6
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-01-01 14:39:21 +0900

    spring

--- (Spring)/newlecSpringMVC/webprj/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
+++ (Spring)/newlecSpringMVC/webprj/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -3,12 +3,21 @@ package com.newlecture.web.controller.customer; // 폴더위의 폴더는 패키
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.newlecture.web.entity.NoticeView;
 import com.newlecture.web.service.NoticeService;
+
+/*
+
+http://localhost:8080/customer/notice/list
+
+*/
 
 @Controller
 @RequestMapping("/customer/notice/") //클래스에url매핑한게 메서드에url매핑과 결합이된다.
@@ -17,12 +26,24 @@ public class NoticeController { // .jsp바로 위의 폴더는 클래스
 	@Autowired
 	private NoticeService noticeService;
 
-	
+/*
 	@RequestMapping("list") //@RequestMapping("/customer/notice/list")  //.jsp가 이제 그저 함수가됐네
-	public String list() throws ClassNotFoundException, SQLException{
-		List<NoticeView> list = noticeService.getList(1, "title", "");
+	public String list(HttpServletRequest request) throws ClassNotFoundException, SQLException{
+		String p = request.getParameter("p");
+		System.out.println(p);
+		
 		return "notice.list";
 	}
+	*/
+
+	
+	@RequestMapping("list") //@RequestMapping("/customer/notice/list")  //.jsp가 이제 그저 함수가됐네
+	public String list(String p) throws ClassNotFoundException, SQLException{
+		System.out.println(p);
+		return "notice.list";
+	}
+	
+	
 	
 	@RequestMapping("detail") //@RequestMapping("/customer/notice/detail")
 	public String detail(){
