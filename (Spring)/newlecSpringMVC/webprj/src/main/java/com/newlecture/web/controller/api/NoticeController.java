8d08c5b1d04f147631f577a65c51eb1d0ec4c9da
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-01-01 14:07:08 +0900

    spring

--- (Spring)/newlecSpringMVC/webprj/src/main/java/com/newlecture/web/controller/api/NoticeController.java
+++ (Spring)/newlecSpringMVC/webprj/src/main/java/com/newlecture/web/controller/api/NoticeController.java
@@ -1,7 +1,14 @@
 package com.newlecture.web.controller.api;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.newlecture.web.entity.NoticeView;
+import com.newlecture.web.service.NoticeService;
 
 /*
 
@@ -15,10 +22,13 @@ http://localhost:8080/api/notice/list
 @RequestMapping("/api/notice/")
 public class NoticeController {
 	
+	@Autowired
+	private NoticeService service;
+	
 	@RequestMapping("list")
-	public String list() {
-		
-		return "공지 list";
+	public List<NoticeView> list() throws ClassNotFoundException, SQLException {
+		List<NoticeView> list = service.getList(1, "TITLE", "");
+		return list;
 	}
 
 }
