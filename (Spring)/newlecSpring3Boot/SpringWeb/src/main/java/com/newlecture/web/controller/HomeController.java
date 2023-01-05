8449ad2c45b0ebc70c9da011a3a97d42429fc62e
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-01-05 11:30:17 +0900

    spring

---
+++ (Spring)/newlecSpring3Boot/SpringWeb/src/main/java/com/newlecture/web/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.newlecture.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@GetMapping("index")
+	@ResponseBody
+	public String index() { 
+		//스프링에서 컨트롤러의 함수명은 사용되지 않는다.
+		// 어노테이션의 url매핑을 찾아서 함수를 호출하니까 함수명은 url매핑과 관련이 없는거다.
+		return "Hello";
+	}
+	
+	@GetMapping("help")
+	@ResponseBody
+	public String help() { 
+		//스프링에서 컨트롤러의 함수명은 사용되지 않는다.
+		// 어노테이션의 url매핑을 찾아서 함수를 호출하니까 함수명은 url매핑과 관련이 없는거다.
+		return "헬프미";
+	}
+	
+
+}
