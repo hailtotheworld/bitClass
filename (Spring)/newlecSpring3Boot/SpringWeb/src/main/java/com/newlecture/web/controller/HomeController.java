458ab73ad408517b6eab66354a5d48f5c2edd5f7
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-01-05 21:02:33 +0900

    spring

--- (Spring)/newlecSpring3Boot/SpringWeb/src/main/java/com/newlecture/web/controller/HomeController.java
+++ (Spring)/newlecSpring3Boot/SpringWeb/src/main/java/com/newlecture/web/controller/HomeController.java
@@ -10,11 +10,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 	
 	@GetMapping("index")
-	@ResponseBody
 	public String index() { 
 		//스프링에서 컨트롤러의 함수명은 사용되지 않는다.
-		// 어노테이션의 url매핑을 찾아서 함수를 호출하니까 함수명은 url매핑과 관련이 없는거다.
-		return "Hello";
+		//어노테이션의 url매핑을 찾아서 함수를 호출하니까 함수명은 url매핑과 관련이 없는거다.
+		return "index.jsp";
 	}
 	
 	@GetMapping("help")
