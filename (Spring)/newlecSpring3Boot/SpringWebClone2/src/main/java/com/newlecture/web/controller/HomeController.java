8faa2703c5f385e85457f986936cf204a2788684
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-01-06 13:27:14 +0900

    spring

--- (Spring)/newlecSpring3Boot/SpringWebClone2/src/main/java/com/newlecture/web/controller/HomeController.java
+++ (Spring)/newlecSpring3Boot/SpringWebClone2/src/main/java/com/newlecture/web/controller/HomeController.java
@@ -3,8 +3,6 @@ package com.newlecture.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/")
@@ -12,13 +10,7 @@ public class HomeController {
 	
 	@GetMapping("index")
 	public String index() {
-		return "index"; 
+		return "index";
 	}
-
+	
 }
-
-
-/*
-spring.mvc.view.prefix=/WEB-INF/view/
-spring.mvc.view.suffix=.jsp
-*/
