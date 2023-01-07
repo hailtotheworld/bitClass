53e1dde8d3b1dcde4fe04a2a7598654826860c5a
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-01-07 11:17:04 +0900

    spring

---
+++ (Spring)/newlecSpring3Boot/SpringWebClone0108/src/main/java/com/newlecture/web/SpringWebClone0107Application.java
@@ -0,0 +1,13 @@
+package com.newlecture.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebClone0107Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebClone0107Application.class, args);
+	}
+
+}
