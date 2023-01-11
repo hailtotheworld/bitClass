e82911a48cdc28451b70bdc71c35202e2ed9229f
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-01-11 16:43:28 +0900

    spring

--- (Spring)/newlecSpring3Boot/SpringWebClone0112/src/main/java/com/newlecture/web/dao/NoticeDao.java
+++ (Spring)/newlecSpring3Boot/SpringWebClone0112/src/main/java/com/newlecture/web/dao/NoticeDao.java
@@ -7,25 +7,19 @@ import org.apache.ibatis.annotations.Mapper;
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.entity.NoticeView;
 
-@Mapper // @Mapper를 읽으면 인터페이스이지만, 객체를 생성해서 IoC컨테이너에 담아준다
+
+@Mapper
 public interface NoticeDao {
 
 	/*
-	@Results({
-	@Result(property = "memberName", column="member_name"),
-	@Result(property = "memberId", column="member_id")
-	})
-	*/
-	/*
-	@Select("select * from noticeview "
+	@Select("SELECT * FROM noticeview "
 			+ "where ${field} like '%${query}%' "
 			+ "order by regdate desc "
-			+ "limit #{offset},#{size}") // #{}은 값이라 양쪽에 ''가 들어간다.  ${}은 그대로 남게된다.
+			+ "limit #{offset},#{size}")
 	List<NoticeView> getList(int offset, int size, String field, String query);
 	*/
 	
-
-	List<NoticeView> getViewList(int offset, int size, String field, String query, boolean pub);
+	List<NoticeView> getViewList(int offset, int size, String field, String query); //pub 넣고 다시해봐
 	int getCount(String field, String query);
 	NoticeView getView(int id);
 
@@ -35,9 +29,8 @@ public interface NoticeDao {
 	int update(Notice notice);
 	int insert(Notice notice);
 	int delete(int id);
-
+	
 	int deleteAll(int[] ids);
 	int updatePubAll(int[] pubIds, int[] closeIds); // close0 pub1 
 //	int updatePubAll(int[] ids, boolean pub); 
-
-} 
+}
---
+++ (Spring)/newlecSpring3Boot/SpringWebClone0112/src/test/java/com/newlecture/web/dao/NoticeDaoTest.java
@@ -0,0 +1,88 @@
+package com.newlecture.web.dao;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@MybatisTest
+class NoticeDaoTest {
+
+	@Autowired
+	NoticeDao noticeDao;
+	/*
+	@Test
+	void getList() {
+		List<NoticeView> list = noticeDao.getViewList(0, 10, "title", "");
+		System.out.println(list);
+	}
+	
+	@Test
+	void getCount() {
+		int result = noticeDao.getCount("title", "제목");
+		System.out.println(result);
+	}
+	
+	@Test
+	void getView() {
+		Notice notice = noticeDao.getView(3);
+		System.out.println(notice);
+	}
+	
+	@Test
+	void getNext() {
+		Notice notice = noticeDao.getNext(6);
+		System.out.println(notice);
+	}
+	
+	@Test
+	void getPrev() {
+		Notice notice = noticeDao.getPrev(6);
+		System.out.println(notice);
+	}
+
+
+	
+	@Test
+	void update() {
+		Notice notice = new Notice();
+		notice.setId(50);
+		notice.setTitle("이것은제목");
+		notice.setContent("이것은내용");
+		notice.setHit(3);
+		notice.setPub(true);
+		
+		int result = noticeDao.update(notice);
+		System.out.println(result);
+	}
+		
+	
+	@Test
+	void insert() {
+		Notice notice = new Notice();
+		notice.setTitle("이것은제목");
+		notice.setContent("이것은내용");
+		notice.setMemberId(1);
+		int result = noticeDao.insert(notice);
+		System.out.println(result);
+	}
+	@Test
+	void delete() {
+		int result = noticeDao.delete(7);
+		System.out.println(result);
+	}
+	*/
+	
+	@Test
+	void updatePubAll() {
+		int[] arr = {};
+		int[] arr2 = {4,5,6};
+		noticeDao.updatePubAll(arr,arr2);
+	}
+
+	
+
+
+}
