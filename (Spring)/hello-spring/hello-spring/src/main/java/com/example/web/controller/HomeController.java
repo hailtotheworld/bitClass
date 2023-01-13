6627c5fa52a146c489599b330a6fb3106c03ccc7
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-01-13 21:49:28 +0900

    spring

---
+++ (Spring)/hello-spring/hello-spring/src/main/java/com/example/web/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("")
+    public String home(Model model) {
+        model.addAttribute("data","template동적인페이지입니다");
+        return "home";
+    }
+
+    @GetMapping("index")
+    public String index(Model model) {
+        model.addAttribute("data","인덱스페이지입니다");
+        return "index";
+    }
+    
+     @GetMapping("hello")
+     public String hello(Model model) {
+         model.addAttribute("data","Spring~!");
+        return "hello"; //  /resources/templates/ hello .html
+     }
+
+
+
+}
