0a211e2a6f9da854bb3c5c7571b3d69184596bef
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-01-17 13:35:46 +0900

    spring

---
+++ (Spring)/hello-spring0118/src/test/java/com/example/web/hellospringclone/HelloSpringCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web.hellospringclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
