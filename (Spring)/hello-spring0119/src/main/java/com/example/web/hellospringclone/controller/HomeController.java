2c74ee29b93c4fb1478b0a5fd0774fa0cf8974e9
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-01-21 10:35:50 +0900

    spring

--- (Spring)/hello-spring0119/src/main/java/com/example/web/hellospringclone/controller/HomeController.java
+++ (Spring)/hello-spring0119/src/main/java/com/example/web/hellospringclone/controller/HomeController.java
@@ -1,67 +1,15 @@
 package com.example.web.hellospringclone.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("index")
-    public String index(Model model) {
-        model.addAttribute("data","안녕 스프링~!");
-        return "index";
-    }
-
-    @GetMapping("helloTemplate")
-    public String helloTemplate(Model model, @RequestParam(required = false, name = "name") String name) {
-        model.addAttribute("name","안녕 스프링~!");
-        return "helloTemplate";
-    }
-
-    @ResponseBody
-    @GetMapping("apiString")
-    public String apiString() {
-        return "문자열 그대로 반환";
-    }
-
-    @ResponseBody
-    @GetMapping("apiJson")
-    public Member apiJson() {
-        Member member = new Member();
-        member.setMemId(1);
-        member.setMemName("첫이름");
-        return member;
-    }
-
-    class Member {
-        private int memId;
-        private String memName;
-
-        public Member() {
-        }
-
-        public Member(int memId, String memName) {
-            this.memId = memId;
-            this.memName = memName;
-        }
-
-        public int getMemId() {
-            return memId;
-        }
-
-        public void setMemId(int memId) {
-            this.memId = memId;
-        }
-
-        public String getMemName() {
-            return memName;
-        }
-
-        public void setMemName(String memName) {
-            this.memName = memName;
-        }
+    @GetMapping("")
+    public String home() {
+        return "home";
     }
 }
--- (Spring)/hello-spring0119/src/main/java/com/example/web/hellospringclone/service/MemberServiceImp.java
+++ (Spring)/hello-spring0119/src/main/java/com/example/web/hellospringclone/service/MemberServiceImp.java
@@ -1,8 +1,10 @@
 package com.example.web.hellospringclone.service;
 
+import com.example.web.hellospringclone.domain.Member;
 import com.example.web.hellospringclone.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 //@Service
 public class MemberServiceImp implements MemberService{
@@ -12,5 +14,28 @@ public class MemberServiceImp implements MemberService{
 //    @Autowired
     public MemberServiceImp(MemberRepository repository) {
         this.repository = repository;
+    }
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+
+        repository.save(member);
+        return member.getId();
+    }
+
+    public List<Member> findMembers() {
+        return repository.findAll();
+    }
+
+    public Optional<Member> findOne(Long id) {
+        return repository.findById(id);
+    }
+
+    public void validateDuplicateMember(Member member) {
+        repository.findByName(member.getName()).ifPresent(member1 -> {throw new IllegalStateException();});
+    }
+
+    public void clearList() {
+        repository.clear();
     }
 }
