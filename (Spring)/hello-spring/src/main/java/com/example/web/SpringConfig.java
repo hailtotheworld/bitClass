6a5f2e9266c065e73edb3cbdfa001b4944f2063f
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-01-21 20:38:40 +0900

    spring

--- (Spring)/hello-spring/src/main/java/com/example/web/SpringConfig.java
+++ (Spring)/hello-spring/src/main/java/com/example/web/SpringConfig.java
@@ -1,13 +1,25 @@
 package com.example.web;
 
+import com.example.web.repository.JdbcMemberRepository;
+import com.example.web.repository.JdbcTemplateMemberRepository;
 import com.example.web.repository.MemberRepository;
-import com.example.web.repository.MemoryMemberRepository;
 import com.example.web.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
+// 조립하는코드. 어셈블리.
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+//    @Autowired //생성자가 1개일때 Autowired 생략해도 된다.
+    public SpringConfig(DataSource dataSource) { //설정파일에서 스프링이 만들어둔 dataSource의 bean을 주입받는거다.
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +28,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return  new MemoryMemberRepository();
+//        return  new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
---
+++ (Spring)/hello-spring/src/test/java/com/example/web/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,88 @@
+package com.example.web.service;
+
+import com.example.web.domain.Member;
+import com.example.web.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@SpringBootTest
+@Transactional //
+class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    /*
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+    */
+
+    /*
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+    */ //DB이용하니까 이제 필요없다.
+
+    @Test
+    public void join() {
+        //given 뭔가가 주어졌는데
+        Member member = new Member();
+        member.setName("spring12");
+
+        //when  이걸로 실행했을때
+        Long saveId = memberService.join(member);
+
+        //then  결과로 이게 나와야돼
+
+
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+    @Test
+    public void validateDuplicateMemberException() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+//        assertThrows(이예외가터져야해, 이 로직을 실행할거야);
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+
+
+//        try {
+//            memberService.join(member2);
+//            fail("예외가 발생해야합니다.");
+//        } catch (IllegalStateException e) {
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+//        }
+
+        // then
+
+    }
+
+    @Test
+    public void findMembers() {
+    }
+
+    @Test
+    public void findOne() {
+    }
+}
