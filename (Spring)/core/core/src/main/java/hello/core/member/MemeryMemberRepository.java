ef438ef34d7d8a6c2e58310cc342a711c46fe553
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-01-27 22:38:08 +0900

    spring

---
+++ (Spring)/core/core/src/main/java/hello/core/member/MemeryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemeryMemberRepository implements MemberRepository {
+
+    // 실무에서는 동시성이슈가 있어서 concurrenthashmap를 사용한다. 예제의 단순화를 위해서 그냥 hashmap썼음
+    private  static Map<Long,Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member); // map.put(key, value); <- key와 value를 넣어서 저장한다.
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId); // map.get(key);        -> key를 넣어서 그에 맞는 value를 반환한다.
+    }
+}
