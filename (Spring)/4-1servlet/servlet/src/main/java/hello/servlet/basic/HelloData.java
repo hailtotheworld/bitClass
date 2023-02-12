f980e792d584a3450465356ebdcf151f5a3249fd
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-02-12 20:04:35 +0900

    spring

---
+++ (Spring)/4-1servlet/servlet/src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,14 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+    //자바빈접근법, 자바프로퍼티접근법
+
+}
