c3991c47e8f086fe6b5418d9d5d90abd59a1797a
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-02-12 14:44:56 +0900

    spring

--- (Spring)/4-1servlet/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ (Spring)/4-1servlet/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //스프링 부트는 서블릿을 직접 등록해서 사용할 수 있도록 @ServletComponentScan 을 지원한다.
 @SpringBootApplication
 public class ServletApplication {
 
---
+++ (Spring)/4-1servlet/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,32 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="helloServlet", urlPatterns="/hello") // 서블릿이름이나 url매핑은 겹치면안된다(중복이 있으면 안된다)
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        //////////////////////////////////////////////////////
+        String username = request.getParameter("username"); // 쿼리파라미터를 편리하게 조회할수가 있다. http메시지에 있는걸 직접 다 파싱해서 읽는다면 굉장히 괴롭지ㅠ
+        System.out.println("username = " + username);
+
+        //////////////////////////////////////////////////////
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8"); // 옛날시스템아니면 지금은 utf-8을 써야한다.
+        response.getWriter().write("hello " + username); //http메시지 바디에 메시지가 들어간다.
+
+
+
+    }
+
+}
