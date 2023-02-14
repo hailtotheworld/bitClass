210d5fb962584cb53e6dd0464cb365eb6797046f
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-02-14 16:07:49 +0900

    spring

--- (Spring)/4-1servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ (Spring)/4-1servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -1,10 +1,11 @@
-package hello.servlet.web.frontcontroller.v3;
+package hello.servlet.web.frontcontroller.v4;
+
 
 import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
-import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
-import hello.servlet.web.frontcontroller.v3.controller.MemberSavaControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSavaControllerV4;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,20 +13,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*") //어떤 url이 들어오든 우선은 일단 프론트컨트롤러에 매핑된다.
-public class FrontControllerServletV3 extends HttpServlet {
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*") //어떤 url이 들어오든 우선은 일단 프론트컨트롤러에 매핑된다.
+public class FrontControllerServletV4 extends HttpServlet {
 
-    private Map<String, ControllerV3> controllerMap = new HashMap<>();//키는 url, 밸류는 컨트롤러
+    private Map<String, ControllerV4> controllerMap = new HashMap<>();//키는 url, 밸류는 컨트롤러
 
-    public FrontControllerServletV3() {
+    public FrontControllerServletV4() {
         // 이 프론트컨트롤러가 생성될때 매핑정보를 담아놓는다.
-        controllerMap.put("/front-controller/v3/members/new-form",new MemberFormControllerV3());
-        controllerMap.put("/front-controller/v3/members/save",new MemberSavaControllerV3());
-        controllerMap.put("/front-controller/v3/members",new MemberListControllerV3());
+        controllerMap.put("/front-controller/v4/members/new-form",new MemberFormControllerV4());
+        controllerMap.put("/front-controller/v4/members/save",new MemberSavaControllerV4());
+        controllerMap.put("/front-controller/v4/members",new MemberListControllerV4());
         // 해당 uri로 접속이오면, 객체를 꺼내쓸수있게 미리 생성해둔거다.
     }
 
@@ -34,19 +34,19 @@ public class FrontControllerServletV3 extends HttpServlet {
 
         String requestURI = request.getRequestURI();
 
-        ControllerV3 controller = controllerMap.get(requestURI);
+        ControllerV4 controller = controllerMap.get(requestURI);
         if(controller == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
 
         Map<String, String> paramMap = createParamMap(request);
-        ModelView mv = controller.process(paramMap);
+        Map<String,Object> model = new HashMap<>(); // (2)여기서 그냥 쓰면된다..!
+        String viewName = controller.process(paramMap, model); // (1) 컨트롤러에서 모델에 값을담은거를
 
-        String viewName = mv.getViewName();//논리이름 new-form
         MyView view = viewResolver(viewName);
 
-        view.render(mv.getModel(), request,response);
+        view.render(model, request, response);
     }
 
     private MyView viewResolver(String viewName) {
