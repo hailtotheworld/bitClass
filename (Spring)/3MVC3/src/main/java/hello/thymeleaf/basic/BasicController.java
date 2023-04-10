fa2b92039a9c585a4cfa704ec31c2f9f3ee215b1
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-04-10 21:10:40 +0900

    spring

--- (Spring)/3MVC3/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ (Spring)/3MVC3/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -115,14 +115,38 @@ public class BasicController {
         return "basic/each";
     }
 
-    private void addUsers(Model model) {
-        List<User> list = new ArrayList<>();
-        list.add(new User("UserA",10));
-        list.add(new User("UserB",20));
-        list.add(new User("UserC",30));
-
-        model.addAttribute("users",list);
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
     }
 
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("UserA", 10));
+        list.add(new User("UserB", 20));
+        list.add(new User("UserC", 30));
+
+        model.addAttribute("users", list);
+    }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("User\"A\"", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
 
 }
