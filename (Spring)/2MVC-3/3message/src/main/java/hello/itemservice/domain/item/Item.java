031feef626b982f0fc39bdccfea54df43b522247
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-04-12 23:43:43 +0900

    spring

--- (Spring)/2MVC-3/3message/src/main/java/hello/itemservice/domain/item/Item.java
+++ (Spring)/2MVC-3/3message/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,8 +2,6 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class Item {
 
@@ -11,11 +9,6 @@ public class Item {
     private String itemName;
     private Integer price;
     private Integer quantity;
-
-    private Boolean open; // 판매 여부
-    private List<String> regions; // 등록 지역 //체크박스라서 여러개선택가능하니까 List
-    private ItemType itemType; // 상품 종류 //라디오버튼이라서 단일선택
-    private String deliveryCode; // 배송 방식
 
     public Item() {
     }
@@ -25,5 +18,4 @@ public class Item {
         this.price = price;
         this.quantity = quantity;
     }
-
 }
