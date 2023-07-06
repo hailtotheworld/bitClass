3ff047d9dad2fe1ffc9e17ef4169f2ea6995c8ef
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-07-06 11:14:37 +0900

    spring

--- (Spring)/0hello-spring/src/main/java/com/example/web/HelloSpringApplication.java
+++ (Spring)/0hello-spring/src/main/java/com/example/web/HelloSpringApplication.java
@@ -1,4 +1,4 @@
-package com.example.web;
+package com.example.web; // 이 패키지 하위만 컴포넌트 스캔을 한다
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
