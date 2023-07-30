d18ad23a22c6dc98827146c7b3c8f03db4bf2142
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-07-30 23:11:00 +0900

    spring

---
+++ (Spring)/2-2MVC/2springmvc_re/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,35 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController // @Controller + @ResponseBody
+public class LogTestController {
+//    private  final Logger log = LoggerFactory.getLogger(LogTestController.class);
+//    private  final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        if (log.isTraceEnabled()) {
+            log.trace("my trace log"+ name);
+        }
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+
+}
