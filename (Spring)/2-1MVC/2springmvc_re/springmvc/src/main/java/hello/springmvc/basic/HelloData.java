e467e6fe60ff29b6671a0d85855d4553170558b9
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-09-14 16:43:26 +0900

    -

---
+++ (Spring)/2-1MVC/2springmvc_re/springmvc/src/main/java/hello/springmvc/basic/HelloData.java
@@ -0,0 +1,20 @@
+package hello.springmvc.basic;
+
+import lombok.Data;
+
+@Data
+public class HelloData {
+
+    private String username;
+    private int age;
+
+    public HelloData() {
+    }
+
+    public HelloData(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+
+}
