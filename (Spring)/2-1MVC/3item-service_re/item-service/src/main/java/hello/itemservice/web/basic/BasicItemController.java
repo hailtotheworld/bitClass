75acd44333d38eb223cf4bafb87569b1eb1f5743
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-09-19 18:53:52 +0900

    -

--- (Spring)/2-1MVC/3item-service_re/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ (Spring)/2-1MVC/3item-service_re/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,8 +5,7 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -20,21 +19,51 @@ public class BasicItemController {
 
     @GetMapping
     public String items(Model model) {
-
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items",items);
         return "basic/items";
-
     }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addFoam() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
 
     /**
      * 테스트용 데이터 추가
      */
     @PostConstruct
     public void init() {
-        itemRepository.save(new Item("itemA",10000,10));
-        itemRepository.save(new Item("itemB",20000,20));
-
+        itemRepository.save(new Item("itemA",11111,11));
+        itemRepository.save(new Item("itemB",22222,22));
+        itemRepository.save(new Item("itemC",33333,33));
     }
 
 
