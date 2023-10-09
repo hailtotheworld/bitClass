50c83318e7fa7c4532f0c3c80cae8305a7f8a2d6
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-10-09 19:47:36 +0900

    -

--- (Spring)/2-2MVC/V2/5login/login/src/main/java/hello/login/web/argumentresolver/LoginMemberArgumentResolver.java
+++ (Spring)/2-2MVC/V2/5login/login/src/main/java/hello/login/web/argumentresolver/LoginMemberArgumentResolver.java
@@ -1,8 +1,10 @@
 package hello.login.web.argumentresolver;
 
 import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
 import hello.login.web.SessionConst;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -14,7 +16,7 @@ import javax.servlet.http.HttpSession;
 
 @Slf4j
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
-    
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         log.info("supportsParameter 실행");
