2610b595bd72d24f8da0ceca7292fd190ffbcf99
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-10-14 00:53:29 +0900

    -

--- (Spring)/2-2MVC/V2/7typeconverter/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
+++ (Spring)/2-2MVC/V2/7typeconverter/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,6 +1,8 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +25,15 @@ public class HelloController {
         return "ok";
     }
 
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT = " + ipPort.getPort());
+        return "ok";
+    }
+
+    @GetMapping("/ip-port/model")
+    public String ipPortModelAttr(@ModelAttribute IpPort ipPort) {
+        return "ok";
+    }
 }
