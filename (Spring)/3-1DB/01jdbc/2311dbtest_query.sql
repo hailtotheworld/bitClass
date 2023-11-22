3bedbd472711ed1aa63baa51020232eebfdcae7e
bitcamp123456 <92347610+bitcamp123456@users.noreply.github.com>
2023-11-22 18:00:12 +0900

    -

---
+++ (Spring)/3-1DB/01jdbc/2311dbtest_query.sql
@@ -0,0 +1,15 @@
+drop table if exists item CASCADE;
+create table item
+(
+id bigint not null AUTO_INCREMENT,
+item_name varchar(10),
+price integer,
+quantity integer,
+primary key (id)
+);
+
+SELECT * FROM item;
+
+insert into item(item_name, price, quantity) values ('ItemTest', 10000, 10);
+
+delete 
